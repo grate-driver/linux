7f34a59cdd1329d40163a4e6c01d268deb220186
Dmitry Osipenko <digetx@gmail.com>
2017-06-18 01:54:20 +0300

    drm/tegra: dc: Correct cursors plane format
    Tegra's cursor format is ARGB8888, which also matches DRM's requirement.
    
    Signed-off-by: Dmitry Osipenko <digetx@gmail.com>

--- drivers/gpu/drm/tegra/dc.c
+++ drivers/gpu/drm/tegra/dc.c
@@ -674,7 +674,7 @@ static struct drm_plane *tegra_dc_primary_plane_create(struct drm_device *drm,
 }
 
 static const u32 tegra_cursor_plane_formats[] = {
-	DRM_FORMAT_RGBA8888,
+	DRM_FORMAT_ARGB8888,
 };
 
 static int tegra_cursor_atomic_check(struct drm_plane *plane,
