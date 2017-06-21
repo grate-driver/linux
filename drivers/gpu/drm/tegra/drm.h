d9b2fdfb9493b87735f96c88f7ef6ea6f9015ac3
Dmitry Osipenko <digetx@gmail.com>
2017-06-22 02:14:41 +0300

    drm/tegra: Dynamically allocate IOVA carveout structure
    Allocate carveout structure dynamically in a preparation for carveout-less
    IOMMU initialization on Tegra20.
    
    Signed-off-by: Dmitry Osipenko <digetx@gmail.com>

--- drivers/gpu/drm/tegra/drm.h
+++ drivers/gpu/drm/tegra/drm.h
@@ -40,6 +40,12 @@ struct tegra_fbdev {
 };
 #endif
 
+struct tegra_drm_carveout {
+	struct iova_domain domain;
+	unsigned long shift;
+	unsigned long limit;
+};
+
 struct tegra_drm {
 	struct drm_device *drm;
 
@@ -47,11 +53,7 @@ struct tegra_drm {
 	struct mutex mm_lock;
 	struct drm_mm mm;
 
-	struct {
-		struct iova_domain domain;
-		unsigned long shift;
-		unsigned long limit;
-	} carveout;
+	struct tegra_drm_carveout *carveout;
 
 	struct mutex clients_lock;
 	struct list_head clients;
