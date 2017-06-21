b332a38b7b66dd07dd31623625dfbfa4ac110f84
Dmitry Osipenko <digetx@gmail.com>
2017-06-22 02:53:47 +0300

    gpu/host1x: Factor out IOMMU initialization
    Factor out IOMMU initialization routine for a general code cleanup and for
    a preparation to make it conditional.
    
    Signed-off-by: Dmitry Osipenko <digetx@gmail.com>

--- drivers/gpu/host1x/dev.c
+++ drivers/gpu/host1x/dev.c
@@ -137,6 +137,51 @@ static const struct of_device_id host1x_of_match[] = {
 };
 MODULE_DEVICE_TABLE(of, host1x_of_match);
 
+static int host1x_iommu_init(struct host1x *host)
+{
+	struct iommu_domain_geometry *geometry;
+	unsigned long order;
+	int err;
+
+	host->group = iommu_group_get(host->dev);
+	if (!host->group)
+		return 0;
+
+	host->domain = iommu_domain_alloc(&platform_bus_type);
+	if (!host->domain)
+		return -ENOMEM;
+
+	err = iommu_attach_device(host->domain, host->dev);
+	if (err == -ENODEV) {
+		iommu_domain_free(host->domain);
+		host->domain = NULL;
+		return 0;
+	}
+
+	err = iommu_attach_group(host->domain, host->group);
+	if (err) {
+		iommu_domain_free(host->domain);
+		host->domain = NULL;
+
+		if (err == -ENODEV) {
+			iommu_group_put(host->group);
+			host->group = NULL;
+			return 0;
+		}
+
+		return err;
+	}
+
+	geometry = &host->domain->geometry;
+
+	order = __ffs(host->domain->pgsize_bitmap);
+	init_iova_domain(&host->iova, 1UL << order,
+			 geometry->aperture_start >> order);
+	host->iova_end = geometry->aperture_end;
+
+	return 0;
+}
+
 static int host1x_probe(struct platform_device *pdev)
 {
 	struct host1x *host;
@@ -218,39 +263,10 @@ static int host1x_probe(struct platform_device *pdev)
 		return err;
 	}
 
-	host->group = iommu_group_get(&pdev->dev);
-	if (host->group) {
-		struct iommu_domain_geometry *geometry;
-		unsigned long order;
+	err = host1x_iommu_init(host);
+	if (err)
+		goto fail_free_domain;
 
-		host->domain = iommu_domain_alloc(&platform_bus_type);
-		if (!host->domain) {
-			err = -ENOMEM;
-			goto put_group;
-		}
-
-		err = iommu_attach_group(host->domain, host->group);
-		if (err) {
-			if (err == -ENODEV) {
-				iommu_domain_free(host->domain);
-				host->domain = NULL;
-				iommu_group_put(host->group);
-				host->group = NULL;
-				goto skip_iommu;
-			}
-
-			goto fail_free_domain;
-		}
-
-		geometry = &host->domain->geometry;
-
-		order = __ffs(host->domain->pgsize_bitmap);
-		init_iova_domain(&host->iova, 1UL << order,
-				 geometry->aperture_start >> order);
-		host->iova_end = geometry->aperture_end;
-	}
-
-skip_iommu:
 	err = host1x_channel_list_init(&host->channel_list,
 				       host->info->nb_channels);
 	if (err) {
@@ -306,10 +322,10 @@ fail_detach_device:
 		iommu_detach_group(host->domain, host->group);
 	}
 fail_free_domain:
-	if (host->domain)
+	if (host->domain) {
 		iommu_domain_free(host->domain);
-put_group:
-	iommu_group_put(host->group);
+		iommu_group_put(host->group);
+	}
 
 	return err;
 }
