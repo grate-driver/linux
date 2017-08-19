5df01afb3d3da478fb1b5b701fec16ddbdc1c2b3
Dmitry Osipenko <digetx@gmail.com>
2017-08-20 00:16:24 +0300

    gpu: host1x: Factor out firewall from job.c
    Now software firewall partially duplicates HW 'gather filter' validations,
    let's factor out it from job.c to get an ability to differentiate Tegra
    generations in order to avoid these duplicated validations and for general
    cleanup. So this patch just moves code around without any functional change.
    
    Signed-off-by: Dmitry Osipenko <digetx@gmail.com>

---
+++ drivers/gpu/host1x/firewall.c
@@ -0,0 +1,228 @@
+/*
+ * Copyright (c) 2012-2015, NVIDIA Corporation.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "dev.h"
+#include "debug.h"
+#include "firewall.h"
+
+#define CDMA_GATHER_MAX_FETCHES_NB 16383
+
+int host1x_firewall_check_job(struct host1x *host, struct host1x_job *job,
+			      struct device *dev)
+{
+	struct host1x_syncpt *sp;
+	unsigned int i;
+
+	sp = host1x_syncpt_get(host, job->syncpt_id);
+	if (!sp) {
+		FW_ERR("Jobs syncpoint ID %u is invalid\n", job->syncpt_id);
+		goto fail;
+	}
+
+	for (i = 0; i < job->num_gathers; i++) {
+		struct host1x_job_gather *g = &job->gathers[i];
+		u64 gather_size = g->words * sizeof(u32);
+
+		/*
+		 * Gather buffer base address must be 4-bytes aligned,
+		 * unaligned offset is malformed and cause commands stream
+		 * corruption on the buffers address relocation.
+		 */
+		if (g->offset & 3) {
+			FW_ERR("Gather #%u has unaligned offset %u\n",
+			       i, g->offset);
+			goto fail;
+		}
+
+		/*
+		 * The maximum number of CDMA gather fetches is 16383, a higher
+		 * value means the words count is malformed.
+		 */
+		if (g->words > CDMA_GATHER_MAX_FETCHES_NB) {
+			FW_ERR("Gather #%u has too many words %u, max %u\n",
+			       i, g->words, CDMA_GATHER_MAX_FETCHES_NB);
+			goto fail;
+		}
+
+		/* Verify that gather is inside of its BO bounds */
+		if (g->offset + gather_size > host1x_bo_size(g->bo)) {
+			FW_ERR("Gather #%u is malformed: offset %u, "
+			       "words %u, BO size %zu\n",
+			       i, g->offset, g->words, host1x_bo_size(g->bo));
+			goto fail;
+		}
+	}
+
+	for (i = 0; i < job->num_relocs; i++) {
+		struct host1x_reloc *reloc = &job->relocarray[i];
+
+		if (reloc->target.offset & 3) {
+			FW_ERR("Relocation #%u has unaligned target "
+			       "offset %lu\n", i, reloc->target.offset);
+			goto fail;
+		}
+
+		if (reloc->target.offset >= host1x_bo_size(reloc->target.bo)) {
+			FW_ERR("Relocation #%u has invalid target "
+			       "offset %lu, max %zu\n",
+			       i, reloc->target.offset,
+			       host1x_bo_size(reloc->target.bo));
+			goto fail;
+		}
+
+		if (reloc->cmdbuf.offset & 3) {
+			FW_ERR("Relocation #%u has unaligned cmdbuf "
+			       "offset %lu\n", i, reloc->cmdbuf.offset);
+			goto fail;
+		}
+
+		if (reloc->cmdbuf.offset >= host1x_bo_size(reloc->cmdbuf.bo)) {
+			FW_ERR("Relocation #%u has invalid cmdbuf "
+			       "offset %lu, max %zu\n",
+			       i, reloc->cmdbuf.offset,
+			       host1x_bo_size(reloc->cmdbuf.bo));
+			goto fail;
+		}
+	}
+
+	for (i = 0; i < job->num_waitchk; i++) {
+		struct host1x_waitchk *wait = &job->waitchk[i];
+
+		sp = host1x_syncpt_get(host, wait->syncpt_id);
+		if (!sp) {
+			FW_ERR("Waitcheck #%u has invalid syncpoint ID %u\n",
+			       i, wait->syncpt_id);
+			goto fail;
+		}
+
+		if (wait->offset & 3) {
+			FW_ERR("Waitcheck #%u has unaligned offset 0x%X\n",
+			       i, wait->offset);
+			goto fail;
+		}
+
+		if (wait->offset >= host1x_bo_size(wait->bo)) {
+			FW_ERR("Waitcheck #%u has invalid offset 0x%X, "
+			       "max %zu\n", i, wait->offset,
+			       host1x_bo_size(wait->bo));
+			goto fail;
+		}
+	}
+
+	return 0;
+
+fail:
+	/* print final error message, giving a clue about jobs client */
+	dev_err(dev, "Job checking failed\n");
+	return -EINVAL;
+}
+
+int host1x_firewall_copy_gathers(struct host1x *host, struct host1x_job *job,
+				 struct device *dev)
+{
+	struct host1x_firewall fw;
+	size_t offset = 0;
+	size_t size = 0;
+	unsigned int i;
+
+	fw.dev = dev;
+	fw.job = job;
+	fw.class = job->class;
+	fw.reloc = job->relocarray;
+	fw.waitchk = job->waitchk;
+	fw.num_relocs = job->num_relocs;
+	fw.num_waitchks = job->num_waitchk;
+	fw.syncpt_incrs = job->syncpt_incrs;
+
+	for (i = 0; i < job->num_gathers; i++)
+		size += job->gathers[i].words * sizeof(u32);
+
+	/*
+	 * Try a non-blocking allocation from a higher priority pools first,
+	 * as awaiting for the allocation here is a major performance hit.
+	 */
+	job->gather_copy_mapped = dma_alloc_wc(dev, size, &job->gather_copy,
+					       GFP_NOWAIT);
+
+	/* the higher priority allocation failed, try the generic-blocking */
+	if (!job->gather_copy_mapped)
+		job->gather_copy_mapped = dma_alloc_wc(dev, size,
+						       &job->gather_copy,
+						       GFP_KERNEL);
+	if (!job->gather_copy_mapped)
+		return -ENOMEM;
+
+	job->gather_copy_size = size;
+
+	for (i = 0; i < job->num_gathers; i++) {
+		struct host1x_job_gather *g = &job->gathers[i];
+		bool last_gather = (i == job->num_gathers - 1);
+		void *gather;
+
+		/* Copy the gather */
+		gather = host1x_bo_mmap(g->bo);
+		memcpy(job->gather_copy_mapped + offset, gather + g->offset,
+		       g->words * sizeof(u32));
+		host1x_bo_munmap(g->bo, gather);
+
+		/* Store the location in the buffer */
+		g->base = job->gather_copy;
+		g->offset = offset;
+
+		/* Validate the job */
+		if (host1x_hw_firewall_validate(host, &fw, g, last_gather)) {
+			/* convert offset to number of words */
+			offset /= sizeof(u32);
+			offset += fw.offset + 1;
+
+			FW_ERR("Debug dump:\n");
+
+			host1x_debug_dump_gather(host, g, offset);
+
+			dev_err(dev, "Command stream validation failed at word "
+				"%u of gather #%d, checked %zu words totally\n",
+				fw.offset, i, offset);
+
+			return -EINVAL;
+		}
+
+		offset += g->words * sizeof(u32);
+	}
+
+	/* No relocs, waitchks and syncpts should remain at this point */
+	if (!fw.num_relocs && !fw.num_waitchks && !fw.syncpt_incrs)
+		return 0;
+
+	FW_ERR("Debug dump:\n");
+
+	for (i = 0; i < job->num_gathers; i++)
+		host1x_debug_dump_gather(host, &job->gathers[i],
+					 CDMA_GATHER_MAX_FETCHES_NB);
+
+	if (fw.num_relocs)
+		FW_ERR("Job has invalid number of relocations, %u left\n",
+		       fw.num_relocs);
+
+	if (fw.num_waitchks)
+		FW_ERR("Job has invalid number of waitchecks, %u left\n",
+		       fw.num_waitchks);
+
+	if (fw.syncpt_incrs)
+		FW_ERR("Job has invalid number of syncpoint increments, "
+		       "%u left\n", fw.syncpt_incrs);
+
+	return -EINVAL;
+}
---
+++ drivers/gpu/host1x/hw/firewall_hw.c
@@ -0,0 +1,331 @@
+/*
+ * Copyright (c) 2012-2015, NVIDIA Corporation.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "../dev.h"
+#include "../firewall.h"
+
+#define HOST1X_INCR_SYNCPT_OFFSET 0x0
+#define HOST1X_WAIT_SYNCPT_OFFSET 0x8
+
+static bool check_reloc(struct host1x_reloc *reloc, struct host1x_bo *cmdbuf,
+			unsigned int offset)
+{
+	offset *= sizeof(u32);
+
+	if (reloc->cmdbuf.bo != cmdbuf) {
+		FW_ERR("Doesn't belong to cmdbuf\n");
+		return false;
+	}
+
+	if (reloc->cmdbuf.offset != offset) {
+		FW_ERR("Invalid command buffer offset 0x%lX\n",
+		       reloc->cmdbuf.offset);
+		return false;
+	}
+
+	/* relocation shift value validation isn't implemented yet */
+	if (reloc->shift) {
+		FW_ERR("Shifting is forbidden\n");
+		return false;
+	}
+
+	return true;
+}
+
+static bool check_wait(struct host1x_waitchk *wait, struct host1x_bo *cmdbuf,
+		       unsigned int offset)
+{
+	offset *= sizeof(u32);
+
+	if (wait->bo != cmdbuf) {
+		FW_ERR("Doesn't belong to cmdbuf\n");
+		return false;
+	}
+
+	if (wait->offset != offset) {
+		FW_ERR("Invalid offset 0x%X\n", wait->offset);
+		return false;
+	}
+
+	return true;
+}
+
+static int check_register(struct host1x_firewall *fw, unsigned long offset,
+			  bool immediate)
+{
+	if (!fw->job->is_addr_reg)
+		return 0;
+
+	if (fw->job->is_addr_reg(fw->dev, offset)) {
+		if (immediate) {
+			FW_ERR("Writing an immediate value to address "
+			       "register\n");
+			return -EINVAL;
+		}
+
+		if (!fw->num_relocs) {
+			FW_ERR("Invalid number of relocations\n");
+			return -EINVAL;
+		}
+
+		if (!check_reloc(fw->reloc, fw->cmdbuf, fw->offset))
+			return -EINVAL;
+
+		fw->num_relocs--;
+		fw->reloc++;
+	}
+
+	/* assume that all modules have INCR_SYNCPT at the same offset */
+	if (HOST1X_HW < 6 && offset == HOST1X_INCR_SYNCPT_OFFSET) {
+		u32 word = fw->cmdbuf_base[fw->offset];
+		unsigned int cond = (word >> 8) & 0xff;
+		unsigned int syncpt_id = word & 0xff;
+		unsigned int i;
+
+		if (!fw->syncpt_incrs) {
+			FW_ERR("Invalid number of syncpoints\n");
+			return -EINVAL;
+		}
+
+		/*
+		 * Syncpoint increment must be the last command of the
+		 * stream in order to ensure that all outstanding HW
+		 * operations complete before jobs fence would signal.
+		 */
+		if (fw->syncpt_incrs == 1) {
+			if (!fw->last || fw->words != (immediate ? 0 : 1)) {
+				FW_ERR("Syncpoint increment must be the last "
+				       "command in a stream\n");
+				return -EINVAL;
+			}
+
+			/* condition must be OP_DONE */
+			if (cond != 1) {
+				FW_ERR("Invalid last syncpoint condition code "
+				       "%u, should be 1 (OP_DONE)\n", cond);
+				return -EINVAL;
+			}
+		}
+
+		/* Check whether syncpoint belongs to jobs client */
+		for (i = 0; i < fw->job->client->num_syncpts; i++) {
+			struct host1x_syncpt *sp = fw->job->client->syncpts[i];
+
+			if (host1x_syncpt_id(sp) == syncpt_id)
+				goto valid_syncpt;
+		}
+
+		FW_ERR("Syncpoint ID %u doesn't belong to the client\n",
+		       syncpt_id);
+
+		return -EINVAL;
+
+valid_syncpt:
+		fw->syncpt_incrs--;
+	}
+
+	if (offset == HOST1X_WAIT_SYNCPT_OFFSET) {
+		if (fw->class != HOST1X_CLASS_HOST1X) {
+			FW_ERR("Jobs class must be 'host1x' for a waitcheck\n");
+			return -EINVAL;
+		}
+
+		if (!fw->num_waitchks) {
+			FW_ERR("Invalid number of a waitchecks\n");
+			return -EINVAL;
+		}
+
+		if (!check_wait(fw->waitchk, fw->cmdbuf, fw->offset))
+			return -EINVAL;
+
+		fw->num_waitchks--;
+		fw->waitchk++;
+	}
+
+	return 0;
+}
+
+static int check_class(struct host1x_firewall *fw, u32 class)
+{
+	if (fw->class != class) {
+		FW_ERR("Invalid class ID 0x%X, should be 0x%X\n",
+		       class, fw->class);
+		return -EINVAL;
+	}
+
+	return 0;
+}
+
+static int check_mask(struct host1x_firewall *fw)
+{
+	u32 mask = fw->mask;
+	u32 reg = fw->reg;
+	int ret;
+
+	while (mask) {
+		if (fw->words == 0) {
+			FW_ERR("Invalid write mask\n");
+			return -EINVAL;
+		}
+
+		if (mask & 1) {
+			ret = check_register(fw, reg, false);
+			if (ret < 0)
+				return ret;
+
+			fw->words--;
+			fw->offset++;
+		}
+		mask >>= 1;
+		reg++;
+	}
+
+	return 0;
+}
+
+static int check_incr(struct host1x_firewall *fw)
+{
+	u32 count = fw->count;
+	u32 reg = fw->reg;
+	int ret;
+
+	while (count) {
+		if (fw->words == 0) {
+			FW_ERR("Invalid words count\n");
+			return -EINVAL;
+		}
+
+		ret = check_register(fw, reg, false);
+		if (ret < 0)
+			return ret;
+
+		reg++;
+		fw->words--;
+		fw->offset++;
+		count--;
+	}
+
+	return 0;
+}
+
+static int check_nonincr(struct host1x_firewall *fw)
+{
+	u32 count = fw->count;
+	int ret;
+
+	while (count) {
+		if (fw->words == 0) {
+			FW_ERR("Invalid words count\n");
+			return -EINVAL;
+		}
+
+		ret = check_register(fw, fw->reg, false);
+		if (ret < 0)
+			return ret;
+
+		fw->words--;
+		fw->offset++;
+		count--;
+	}
+
+	return 0;
+}
+
+static int firewall_validate_gather(struct host1x_firewall *fw,
+				    struct host1x_job_gather *g,
+				    bool last_gather)
+{
+	u32 *cmdbuf_base = (u32 *)fw->job->gather_copy_mapped +
+		(g->offset / sizeof(u32));
+	int err = 0;
+
+	fw->cmdbuf_base = cmdbuf_base;
+	fw->last = last_gather;
+	fw->words = g->words;
+	fw->cmdbuf = g->bo;
+	fw->offset = 0;
+
+	if (!fw->syncpt_incrs) {
+		FW_ERR("Invalid number of syncpoints\n");
+		return -EINVAL;
+	}
+
+	while (fw->words && !err) {
+		u32 word = cmdbuf_base[fw->offset];
+		u32 opcode = (word & 0xf0000000) >> 28;
+
+		fw->mask = 0;
+		fw->reg = 0;
+		fw->count = 0;
+		fw->words--;
+		fw->offset++;
+
+		switch (opcode) {
+		case HOST1X_OPCODE_SETCLASS:
+			fw->mask = word & 0x3f;
+			fw->reg = word >> 16 & 0xfff;
+			err = check_class(fw, word >> 6 & 0x3ff);
+			if (!err)
+				err = check_mask(fw);
+			break;
+
+		case HOST1X_OPCODE_INCR:
+			fw->reg = word >> 16 & 0xfff;
+			fw->count = word & 0xffff;
+			err = check_incr(fw);
+			break;
+
+		case HOST1X_OPCODE_NONINCR:
+			fw->reg = word >> 16 & 0xfff;
+			fw->count = word & 0xffff;
+			err = check_nonincr(fw);
+			break;
+
+		case HOST1X_OPCODE_MASK:
+			fw->mask = word & 0xffff;
+			fw->reg = word >> 16 & 0xfff;
+			err = check_mask(fw);
+			break;
+
+		case HOST1X_OPCODE_IMM:
+			fw->reg = word >> 16 & 0x1fff;
+			err = check_register(fw, fw->reg, true);
+			if (err)
+				fw->offset--;
+			break;
+
+		case HOST1X_OPCODE_RESTART:
+		case HOST1X_OPCODE_GATHER:
+		case HOST1X_OPCODE_EXTEND:
+			FW_ERR("Forbidden command\n");
+			err = -EINVAL;
+			fw->offset--;
+			break;
+
+		default:
+			FW_ERR("Invalid command\n");
+			err = -EINVAL;
+			fw->offset--;
+			break;
+		}
+	}
+
+	return err;
+}
+
+static const struct host1x_firewall_ops host1x_firewall_ops = {
+	.validate_gather = firewall_validate_gather,
+};
