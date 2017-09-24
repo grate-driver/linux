2d1e78ca8bfe1a10c2d7fd6423c027195cb861db
Dmitry Osipenko <digetx@gmail.com>
2017-09-24 04:58:27 +0300

    drm/tegra: gr2d: Implement HW context switching
    Implement HW context switching for GR2D. GR2D has 5 G2 and 3 SB HW context
    register files. One G2 and one SB HW context are traded for an unlimited
    number of SW contexts, context store would be performed for these traded
    contexts.
    
    Signed-off-by: Dmitry Osipenko <digetx@gmail.com>

--- drivers/gpu/drm/tegra/gr2d.c
+++ drivers/gpu/drm/tegra/gr2d.c
@@ -13,6 +13,36 @@
 #include "gem.h"
 #include "gr2d.h"
 
+#define GR2D_SW_CTX_G2_CLASS	HOST1X_CLASS_GR2D_G2_CTX5
+#define GR2D_SW_CTX_SB_CLASS	HOST1X_CLASS_GR2D_SB_CTX3
+
+#define OPCODE_SETCL(classid)				\
+	((0x0 << 28) | (classid << 6))
+
+#define OPCODE_NONINCR(offset, count)			\
+	((0x2 << 28) | (offset << 16) | count)
+
+#define OPCODE_IMM(offset, data)			\
+	((0x4 << 28) | (offset << 16) | data)
+
+#define OPCODE_GATHER(offset, insert, incr, count)	\
+	((0x6 << 28) | (offset << 16) | (insert << 15) | (incr << 14) | count)
+
+#define OPCODE_NOP		OPCODE_NONINCR(0, 0)
+
+#define INDREAD(modid, offset, autoinc)			\
+	((autoinc << 27) | (modid << 18) | ((offset) << 2) | 1)
+
+#define GR2D_REGINFO(offset, count)			\
+{							\
+	offset, count,					\
+}
+
+struct gr2d_regs_desc {
+	unsigned int offset;
+	unsigned int count;
+};
+
 struct gr2d {
 	struct tegra_drm_client client;
 	struct host1x_channel *channel;
@@ -20,6 +50,24 @@ struct gr2d {
 	struct clk *clk;
 
 	DECLARE_BITMAP(addr_regs, GR2D_NUM_REGS);
+
+	unsigned long g2_contexts;
+	unsigned long sb_contexts;
+};
+
+static const struct gr2d_regs_desc gr2d_regs[] = {
+	GR2D_REGINFO(0x0c,  1),
+	GR2D_REGINFO(0x11,  9),
+	GR2D_REGINFO(0x1a,  2),
+	GR2D_REGINFO(0x1c, 10),
+	GR2D_REGINFO(0x26,  1),
+	GR2D_REGINFO(0x27,  4),
+	GR2D_REGINFO(0x2b,  3),
+	GR2D_REGINFO(0x2e,  3),
+	GR2D_REGINFO(0x31,  2),
+	GR2D_REGINFO(0x33, 20),
+	GR2D_REGINFO(0x47,  6),
+	GR2D_REGINFO(0x09,  3),
 };
 
 static inline struct gr2d *to_gr2d(struct tegra_drm_client *client)
@@ -93,25 +141,269 @@ static const struct host1x_client_ops gr2d_client_ops = {
 	.reset = gr2d_reset,
 };
 
+static int gr2d_allocate_ctx(struct host1x_client *client,
+			     struct host1x_bo **bo)
+{
+	struct drm_device *dev = dev_get_drvdata(client->parent);
+	struct tegra_drm *tegra = dev->dev_private;
+	struct tegra_bo *obj;
+
+	obj = tegra_bo_create(tegra->drm, SZ_1K, 0);
+	if (!obj)
+		return -ENOMEM;
+
+	*bo =  &obj->base;
+
+	return 0;
+}
+
+static int gr2d_initialize_ctx(struct host1x_client *client,
+			       u32 class,
+			       u32 *bo_vaddr,
+			       dma_addr_t bo_dma,
+			       u32 *bo_offset,
+			       unsigned int *words_num,
+			       struct host1x_context_push_data **restore_data,
+			       struct host1x_context_push_data **store_data,
+			       unsigned int *restore_pushes,
+			       unsigned int *store_pushes)
+{
+	struct host1x_context_push_data *restore;
+	struct host1x_context_push_data *store;
+	const struct gr2d_regs_desc *regs;
+	unsigned int pushes_num;
+	unsigned int offset;
+	unsigned int count;
+	unsigned int words;
+	unsigned int base;
+	unsigned int i;
+
+	pushes_num = ARRAY_SIZE(gr2d_regs) * 2 + 1;
+
+	store = kmalloc_array(pushes_num, sizeof(*store), GFP_KERNEL);
+	if (!store)
+		return -ENOMEM;
+
+	*store_data = store;
+	*store_pushes = pushes_num;
+
+	pushes_num = ARRAY_SIZE(gr2d_regs) + 2;
+
+	restore = kmalloc_array(pushes_num, sizeof(*restore), GFP_KERNEL);
+	if (!restore) {
+		kfree(store);
+		return -ENOMEM;
+	}
+
+	*restore_data = restore;
+	*restore_pushes = pushes_num;
+
+	restore->word0 = OPCODE_SETCL(class);
+	restore->word1 = OPCODE_NOP;
+	restore++;
+
+	/* first reset trigger registers, they will be restored in the end */
+	restore->word0 = OPCODE_GATHER(0, 0, 0, 3);
+	restore->word1 = bo_dma;
+	restore++;
+
+	bo_vaddr[0] = OPCODE_IMM(0x9, 0);
+	bo_vaddr[1] = OPCODE_IMM(0xa, 0);
+	bo_vaddr[2] = OPCODE_IMM(0xb, 0);
+
+	store->word0 = OPCODE_SETCL(HOST1X_CLASS_HOST1X);
+	store->word1 = OPCODE_NOP;
+	store++;
+
+	/* point base to the start of contexts registers bank */
+	base = (class & 0xf) * 0x1000;
+
+	for (i = words = 0, regs = gr2d_regs; i < ARRAY_SIZE(gr2d_regs);
+	     i++, regs++, words += count) {
+		offset = regs->offset;
+		count = regs->count;
+
+		/* store: setup indirect registers access pointer */
+		store->word0 = OPCODE_NONINCR(0x2d, 1);
+		store->word1 = INDREAD(HOST1X_MODULE_GR2D, base + offset, true);
+		store++;
+
+		/* store: indirectly read 2d regs and push them to 'out' FIFO */
+		store->word0 = OPCODE_GATHER(0x2e, 1, 0, count);
+		store->word1 = bo_dma;
+		store++;
+
+		/* restore: fetch data from BO and write it indirectly to 2d */
+		restore->word0 = OPCODE_GATHER(offset, 1, true, count);
+		restore->word1 = bo_dma + (3 + words) * sizeof(u32);
+		restore++;
+	}
+
+	*words_num = words;
+	/* adjust BO address, skipping trigger registers reset */
+	*bo_offset = 3 * sizeof(u32);
+
+	return 0;
+}
+
+static void gr2d_debug_ctx(struct host1x_client *client, u32 *bo_vaddr)
+{
+	struct device *dev = client->dev;
+	const struct gr2d_regs_desc *regs;
+	unsigned int offset;
+	unsigned int count;
+	unsigned int words;
+	unsigned int i, k;
+	u32 data;
+
+	if (!(drm_debug & DRM_UT_DRIVER))
+		return;
+
+	for (i = 0; i < 3; i++) {
+		offset = (bo_vaddr[i] & GENMASK(27, 16)) >> 16;
+		data   = (bo_vaddr[i] & GENMASK(15,  0)) >>  0;
+
+		DRM_DEV_DEBUG_DRIVER(dev, "%p[%u] [%03X] <= %08X\n",
+				     bo_vaddr, i, offset, data);
+	}
+
+	for (i = 0, words = 3, regs = gr2d_regs; i < ARRAY_SIZE(gr2d_regs);
+	     i++, regs++, words += count) {
+		offset = regs->offset;
+		count = regs->count;
+
+		DRM_DEV_DEBUG_DRIVER(dev, "%p[%u] offset %03X count %u\n",
+				     bo_vaddr, words, offset, count);
+
+		for (k = 0; k < count; k++, offset++) {
+			DRM_DEV_DEBUG_DRIVER(dev, "%p[%u] [%03X] <= %08X\n",
+					     bo_vaddr, words + k, offset,
+					     bo_vaddr[words + k]);
+		}
+	}
+}
+
+static const struct host1x_context_ops gr2d_context_ops = {
+	.initialize = gr2d_initialize_ctx,
+	.allocate = gr2d_allocate_ctx,
+	.debug = gr2d_debug_ctx,
+};
+
+static int gr2d_get_context(struct gr2d *gr2d, enum drm_tegra_client client,
+			    u32 *class)
+{
+	int index;
+
+	/*
+	 * There are 5 G2 contexts and 3 SB contexts, we would trade 1 G2
+	 * and 1 SB HW context for a software 'switchable' contexts in
+	 * order to have unlimited number of contexts.
+	 */
+	switch (client) {
+	case DRM_TEGRA_CLIENT_GR2D_G2:
+		index = find_first_zero_bit(&gr2d->g2_contexts, 4);
+		index = min(index, 4);
+
+		switch (index) {
+		case 0: *class = HOST1X_CLASS_GR2D_G2_CTX1; break;
+		case 1: *class = HOST1X_CLASS_GR2D_G2_CTX2; break;
+		case 2: *class = HOST1X_CLASS_GR2D_G2_CTX3; break;
+		case 3: *class = HOST1X_CLASS_GR2D_G2_CTX4; break;
+		default: *class = GR2D_SW_CTX_G2_CLASS; break;
+		}
+
+		set_bit(index, &gr2d->g2_contexts);
+		break;
+
+	case DRM_TEGRA_CLIENT_GR2D_SB:
+		index = find_first_zero_bit(&gr2d->sb_contexts, 2);
+		index = min(index, 2);
+
+		switch (index) {
+		case 0: *class = HOST1X_CLASS_GR2D_SB_CTX1; break;
+		case 1: *class = HOST1X_CLASS_GR2D_SB_CTX2; break;
+		default: *class = GR2D_SW_CTX_SB_CLASS; break;
+		}
+
+		set_bit(index, &gr2d->sb_contexts);
+		break;
+
+	default:
+		return -ENODEV;
+	}
+
+	return 0;
+}
+
+static void gr2d_release_context(struct tegra_drm_context *context)
+{
+	struct gr2d *gr2d = to_gr2d(context->client);
+	struct host1x_client *client = &context->client->base;
+	u32 class = host1x_context_class(client, context->hwctx);
+	bool g2ctx = false;
+	bool sbctx = false;
+	int index;
+
+	switch (class) {
+	case HOST1X_CLASS_GR2D_G2_CTX1: g2ctx = true; index = 0; break;
+	case HOST1X_CLASS_GR2D_G2_CTX2: g2ctx = true; index = 1; break;
+	case HOST1X_CLASS_GR2D_G2_CTX3: g2ctx = true; index = 2; break;
+	case HOST1X_CLASS_GR2D_G2_CTX4: g2ctx = true; index = 3; break;
+	case HOST1X_CLASS_GR2D_G2_CTX5: g2ctx = true; index = 4; break;
+	case HOST1X_CLASS_GR2D_SB_CTX1: sbctx = true; index = 0; break;
+	case HOST1X_CLASS_GR2D_SB_CTX2: sbctx = true; index = 1; break;
+	case HOST1X_CLASS_GR2D_SB_CTX3: sbctx = true; index = 2; break;
+
+	default:
+		WARN(1, "Invalid class 0x%X\n", class);
+		return;
+	}
+
+	if (g2ctx)
+		clear_bit(index, &gr2d->g2_contexts);
+
+	if (sbctx)
+		clear_bit(index, &gr2d->sb_contexts);
+}
+
 static int gr2d_open_channel(struct tegra_drm_client *client,
 			     struct tegra_drm_context *context,
 			     enum drm_tegra_client clientid)
 {
 	struct gr2d *gr2d = to_gr2d(client);
+	struct host1x_client *cl = &client->base;
+	struct host1x_syncpt *sp = cl->syncpts[0];
+	bool sw_ctx = false;
+	u32 class;
+	int err;
 
-	if (clientid != DRM_TEGRA_CLIENT_GR2D_G2 &&
-	    clientid != DRM_TEGRA_CLIENT_GR2D_SB)
-		return -ENODEV;
+	err = gr2d_get_context(gr2d, clientid, &class);
+	if (err < 0)
+		return err;
+
+	if (class == GR2D_SW_CTX_G2_CLASS ||
+	    class == GR2D_SW_CTX_SB_CLASS)
+		sw_ctx = true;
 
 	context->channel = host1x_channel_get(gr2d->channel);
 	if (!context->channel)
 		return -ENOMEM;
+
+	context->hwctx = host1x_create_context(&gr2d_context_ops,
+					       context->channel, cl, sp,
+					       class, sw_ctx, false, sw_ctx);
+	if (IS_ERR(context->hwctx)) {
+		host1x_channel_put(context->channel);
+		return PTR_ERR(context->hwctx);
+	}
 
 	return 0;
 }
 
 static void gr2d_close_channel(struct tegra_drm_context *context)
 {
+	gr2d_release_context(context);
+	host1x_context_put(context->hwctx);
 	host1x_channel_put(context->channel);
 }
 
