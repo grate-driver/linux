86766d4b6cdb85169b487c82eb4ef8273cd166ce
Andrew Morton <akpm@linux-foundation.org>
2017-10-09 21:24:48 +0100

    sysvipc-make-get_maxid-o1-again-checkpatch-fixes
    ERROR: space required before the open parenthesis '('
    #110: FILE: ipc/util.c:439:
    +			if(lid == -1)
    
    total: 1 errors, 0 warnings, 113 lines checked
    
    NOTE: For some of the reported defects, checkpatch may be able to
          mechanically convert to the typical style using --fix or --fix-inplace.
    
    ./patches/sysvipc-make-get_maxid-o1-again.patch has style problems, please review.
    
    NOTE: If any of the errors are false positives, please report
          them to the maintainer, see CHECKPATCH in MAINTAINERS.
    
    Please run checkpatch prior to sending patches
    
    Cc: Davidlohr Bueso <dave@stgolabs.net>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- ipc/util.c
+++ ipc/util.c
@@ -436,7 +436,7 @@ void ipc_rmid(struct ipc_ids *ids, struct kern_ipc_perm *ipcp)
 	if (unlikely(lid == ids->max_id)) {
 		do {
 			lid--;
-			if(lid == -1)
+			if (lid == -1)
 				break;
 		} while (!idr_find(&ids->ipcs_idr, lid));
 		ids->max_id = lid;
