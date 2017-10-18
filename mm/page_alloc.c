e51002507df5df61bd19109d7b9a6ae7c0876e0a
Andrew Morton <akpm@linux-foundation.org>
2017-10-18 17:46:36 +0100

    mm-drop-migrate-type-checks-from-has_unmovable_pages-fix
    remove unused local `mt'
    
    Cc: Michal Hocko <mhocko@suse.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -7352,7 +7352,6 @@ bool has_unmovable_pages(struct zone *zone, struct page *page, int count,
 			 bool skip_hwpoisoned_pages)
 {
 	unsigned long pfn, iter, found;
-	int mt;
 
 	/*
 	 * For avoiding noise data, lru_add_drain_all() should be called
