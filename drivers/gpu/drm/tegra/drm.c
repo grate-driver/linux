ea08264dc0a88f18124d03919ebbc6e786878e25
Dmitry Osipenko <digetx@gmail.com>
2017-11-16 20:05:06 +0300

    drm/tegra: Refcount contexts
    Add context refcount facility that will be used by further patches.
    
    Signed-off-by: Dmitry Osipenko <digetx@gmail.com>

--- drivers/gpu/drm/tegra/drm.c
+++ drivers/gpu/drm/tegra/drm.c
@@ -382,10 +382,23 @@ static int tegra_drm_open(struct drm_device *drm, struct drm_file *filp)
 	return 0;
 }
 
-static void tegra_drm_context_free(struct tegra_drm_context *context)
+static void tegra_drm_context_free(struct kref *ref)
 {
+	struct tegra_drm_context *context =
+		container_of(ref, struct tegra_drm_context, ref);
+
 	context->client->ops->close_channel(context);
 	kfree(context);
+}
+
+static void tegra_drm_context_get(struct tegra_drm_context *context)
+{
+	kref_get(&context->ref);
+}
+
+static void tegra_drm_context_put(struct tegra_drm_context *context)
+{
+	kref_put(&context->ref, tegra_drm_context_free);
 }
 
 static struct host1x_bo *
@@ -1141,6 +1154,7 @@ static int tegra_client_open(struct tegra_drm_file *fpriv,
 		return err;
 	}
 
+	kref_init(&context->ref);
 	context->client = client;
 	context->id = err;
 
@@ -1197,7 +1211,7 @@ static int tegra_close_channel(struct drm_device *drm, void *data,
 	}
 
 	idr_remove(&fpriv->contexts, context->id);
-	tegra_drm_context_free(context);
+	tegra_drm_context_put(context);
 
 unlock:
 	mutex_unlock(&fpriv->lock);
@@ -1519,7 +1533,7 @@ static int tegra_drm_context_cleanup(int id, void *p, void *data)
 {
 	struct tegra_drm_context *context = p;
 
-	tegra_drm_context_free(context);
+	tegra_drm_context_put(context);
 
 	return 0;
 }
