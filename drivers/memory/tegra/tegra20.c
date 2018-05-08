737a5186f8c22cc82b354e5c8b2742b3d2a10daf
Dmitry Osipenko <digetx@gmail.com>
2018-05-08 16:28:36 +0300

    memory: tegra: Block DMA for clients HW on a faulty memory access
    Currently Memory Controller informs about erroneous memory accesses done
    by memory clients and that's it. Let's make it to block whole HW unit that
    corresponds to the misbehaving memory client in order to try to avoid
    memory corruptions and to stop deliberate attempts of manipulation by a
    misbehaving client.
    
    Signed-off-by: Dmitry Osipenko <digetx@gmail.com>

--- drivers/memory/tegra/tegra20.c
+++ drivers/memory/tegra/tegra20.c
@@ -15,159 +15,211 @@ static const struct tegra_mc_client tegra20_mc_clients[] = {
 	{
 		.id = 0x00,
 		.name = "display0a",
+		.reset_id = TEGRA_MC_CLIENT_NO_RESET,
 	}, {
 		.id = 0x01,
 		.name = "display0ab",
+		.reset_id = TEGRA_MC_CLIENT_NO_RESET,
 	}, {
 		.id = 0x02,
 		.name = "display0b",
+		.reset_id = TEGRA_MC_CLIENT_NO_RESET,
 	}, {
 		.id = 0x03,
 		.name = "display0bb",
+		.reset_id = TEGRA_MC_CLIENT_NO_RESET,
 	}, {
 		.id = 0x04,
 		.name = "display0c",
+		.reset_id = TEGRA_MC_CLIENT_NO_RESET,
 	}, {
 		.id = 0x05,
 		.name = "display0cb",
+		.reset_id = TEGRA_MC_CLIENT_NO_RESET,
 	}, {
 		.id = 0x06,
 		.name = "display1b",
+		.reset_id = TEGRA_MC_CLIENT_NO_RESET,
 	}, {
 		.id = 0x07,
 		.name = "display1bb",
+		.reset_id = TEGRA_MC_CLIENT_NO_RESET,
 	}, {
 		.id = 0x08,
 		.name = "eppup",
+		.reset_id = TEGRA20_MC_RESET_EPP,
 	}, {
 		.id = 0x09,
 		.name = "g2pr",
+		.reset_id = TEGRA20_MC_RESET_2D,
 	}, {
 		.id = 0x0a,
 		.name = "g2sr",
+		.reset_id = TEGRA20_MC_RESET_2D,
 	}, {
 		.id = 0x0b,
 		.name = "mpeunifbr",
+		.reset_id = TEGRA20_MC_RESET_MPEB,
 	}, {
 		.id = 0x0c,
 		.name = "viruv",
+		.reset_id = TEGRA20_MC_RESET_VI,
 	}, {
 		.id = 0x0d,
 		.name = "avpcarm7r",
+		.reset_id = TEGRA_MC_CLIENT_NO_RESET,
 	}, {
 		.id = 0x0e,
 		.name = "displayhc",
+		.reset_id = TEGRA_MC_CLIENT_NO_RESET,
 	}, {
 		.id = 0x0f,
 		.name = "displayhcb",
+		.reset_id = TEGRA_MC_CLIENT_NO_RESET,
 	}, {
 		.id = 0x10,
 		.name = "fdcdrd",
+		.reset_id = TEGRA20_MC_RESET_3D,
 	}, {
 		.id = 0x11,
 		.name = "g2dr",
+		.reset_id = TEGRA20_MC_RESET_2D,
 	}, {
 		.id = 0x12,
 		.name = "host1xdmar",
+		.reset_id = TEGRA20_MC_RESET_HC,
 	}, {
 		.id = 0x13,
 		.name = "host1xr",
+		.reset_id = TEGRA20_MC_RESET_HC,
 	}, {
 		.id = 0x14,
 		.name = "idxsrd",
+		.reset_id = TEGRA20_MC_RESET_3D,
 	}, {
 		.id = 0x15,
 		.name = "mpcorer",
+		.reset_id = TEGRA_MC_CLIENT_NO_RESET,
 	}, {
 		.id = 0x16,
 		.name = "mpe_ipred",
+		.reset_id = TEGRA_MC_CLIENT_NO_RESET,
 	}, {
 		.id = 0x17,
 		.name = "mpeamemrd",
+		.reset_id = TEGRA20_MC_RESET_MPEA,
 	}, {
 		.id = 0x18,
 		.name = "mpecsrd",
+		.reset_id = TEGRA20_MC_RESET_MPEC,
 	}, {
 		.id = 0x19,
 		.name = "ppcsahbdmar",
+		.reset_id = TEGRA_MC_CLIENT_NO_RESET,
 	}, {
 		.id = 0x1a,
 		.name = "ppcsahbslvr",
+		.reset_id = TEGRA_MC_CLIENT_NO_RESET,
 	}, {
 		.id = 0x1b,
 		.name = "texsrd",
+		.reset_id = TEGRA20_MC_RESET_3D,
 	}, {
 		.id = 0x1c,
 		.name = "vdebsevr",
+		.reset_id = TEGRA20_MC_RESET_VDE,
 	}, {
 		.id = 0x1d,
 		.name = "vdember",
+		.reset_id = TEGRA20_MC_RESET_VDE,
 	}, {
 		.id = 0x1e,
 		.name = "vdemcer",
+		.reset_id = TEGRA20_MC_RESET_VDE,
 	}, {
 		.id = 0x1f,
 		.name = "vdetper",
+		.reset_id = TEGRA20_MC_RESET_VDE,
 	}, {
 		.id = 0x20,
 		.name = "eppu",
+		.reset_id = TEGRA20_MC_RESET_EPP,
 	}, {
 		.id = 0x21,
 		.name = "eppv",
+		.reset_id = TEGRA20_MC_RESET_EPP,
 	}, {
 		.id = 0x22,
 		.name = "eppy",
+		.reset_id = TEGRA20_MC_RESET_EPP,
 	}, {
 		.id = 0x23,
 		.name = "mpeunifbw",
+		.reset_id = TEGRA20_MC_RESET_MPEB,
 	}, {
 		.id = 0x24,
 		.name = "viwsb",
+		.reset_id = TEGRA20_MC_RESET_VI,
 	}, {
 		.id = 0x25,
 		.name = "viwu",
+		.reset_id = TEGRA20_MC_RESET_VI,
 	}, {
 		.id = 0x26,
 		.name = "viwv",
+		.reset_id = TEGRA20_MC_RESET_VI,
 	}, {
 		.id = 0x27,
 		.name = "viwy",
+		.reset_id = TEGRA20_MC_RESET_VI,
 	}, {
 		.id = 0x28,
 		.name = "g2dw",
+		.reset_id = TEGRA20_MC_RESET_2D,
 	}, {
 		.id = 0x29,
 		.name = "avpcarm7w",
+		.reset_id = TEGRA_MC_CLIENT_NO_RESET,
 	}, {
 		.id = 0x2a,
 		.name = "fdcdwr",
+		.reset_id = TEGRA20_MC_RESET_3D,
 	}, {
 		.id = 0x2b,
 		.name = "host1xw",
+		.reset_id = TEGRA20_MC_RESET_HC,
 	}, {
 		.id = 0x2c,
 		.name = "ispw",
+		.reset_id = TEGRA20_MC_RESET_ISP,
 	}, {
 		.id = 0x2d,
 		.name = "mpcorew",
+		.reset_id = TEGRA_MC_CLIENT_NO_RESET,
 	}, {
 		.id = 0x2e,
 		.name = "mpecswr",
+		.reset_id = TEGRA20_MC_RESET_MPEC,
 	}, {
 		.id = 0x2f,
 		.name = "ppcsahbdmaw",
+		.reset_id = TEGRA_MC_CLIENT_NO_RESET,
 	}, {
 		.id = 0x30,
 		.name = "ppcsahbslvw",
+		.reset_id = TEGRA_MC_CLIENT_NO_RESET,
 	}, {
 		.id = 0x31,
 		.name = "vdebsevw",
+		.reset_id = TEGRA20_MC_RESET_VDE,
 	}, {
 		.id = 0x32,
 		.name = "vdembew",
+		.reset_id = TEGRA20_MC_RESET_VDE,
 	}, {
 		.id = 0x33,
 		.name = "vdetpmw",
+		.reset_id = TEGRA20_MC_RESET_VDE,
 	},
 };
 
