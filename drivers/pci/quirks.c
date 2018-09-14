f4b2d6de46457976fac5e1068fe5159f854e377f
Nicolas Chauvet <kwizart@gmail.com>
2018-09-14 14:59:15 +0200

    PCI: disable nv_msi_ht_cap_quirk_leaf quirk on arm/arm64
    This patch disable the use of nv_msi_ht_cap_quirk_leaf quirk on arm and
    arm64 NVIDIA devices such as Tegra
    
    This fixes the following output:
    "pci 0000:00:01.0: nv_msi_ht_cap_quirk didn't locate host bridge"
    as experienced on a Trimslice device with PCI host bridge enabled
    
    v3: exclude the quirk for arm and arm64 instead of only for x86
    
    v2: use __maybe_unused to avoid a warning on nv_msi_ht_cap_quirk_leaf
    
    Signed-off-by: Nicolas Chauvet <kwizart@gmail.com>
    Reviewed-by: Manikanta Maddireddy <mmaddireddy@nvidia.com>
    Acked-by: Thierry Reding <treding@nvidia.com>

--- drivers/pci/quirks.c
+++ drivers/pci/quirks.c
@@ -2954,12 +2954,15 @@ static void nv_msi_ht_cap_quirk_all(struct pci_dev *dev)
 DECLARE_PCI_FIXUP_FINAL(PCI_VENDOR_ID_AL, PCI_ANY_ID, nv_msi_ht_cap_quirk_all);
 DECLARE_PCI_FIXUP_RESUME_EARLY(PCI_VENDOR_ID_AL, PCI_ANY_ID, nv_msi_ht_cap_quirk_all);
 
-static void nv_msi_ht_cap_quirk_leaf(struct pci_dev *dev)
+static void __maybe_unused nv_msi_ht_cap_quirk_leaf(struct pci_dev *dev)
 {
 	return __nv_msi_ht_cap_quirk(dev, 0);
 }
+/* HyperTransport is not relevant on theses arches */
+#if !IS_ENABLED(CONFIG_ARM) && !IS_ENABLED(CONFIG_ARM64)
 DECLARE_PCI_FIXUP_FINAL(PCI_VENDOR_ID_NVIDIA, PCI_ANY_ID, nv_msi_ht_cap_quirk_leaf);
 DECLARE_PCI_FIXUP_RESUME_EARLY(PCI_VENDOR_ID_NVIDIA, PCI_ANY_ID, nv_msi_ht_cap_quirk_leaf);
+#endif
 
 static void quirk_msi_intx_disable_bug(struct pci_dev *dev)
 {
