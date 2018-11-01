a357bdd2a69e9731f4e4bef1117944f49aef3175
David Howells <dhowells@redhat.com>
2018-11-01 23:07:26 +0000

    vfs: Remove kern_mount_data()
    The kern_mount_data() isn't used any more so remove it.
    
    Signed-off-by: David Howells <dhowells@redhat.com>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -2275,8 +2275,7 @@ mount_pseudo(struct file_system_type *fs_type, char *name,
 
 extern int register_filesystem(struct file_system_type *);
 extern int unregister_filesystem(struct file_system_type *);
-#define kern_mount(type) kern_mount_data(type, NULL)
-extern struct vfsmount *kern_mount_data(struct file_system_type *, void *);
+extern struct vfsmount *kern_mount(struct file_system_type *);
 extern void kern_unmount(struct vfsmount *mnt);
 extern int may_umount_tree(struct vfsmount *);
 extern int may_umount(struct vfsmount *);
