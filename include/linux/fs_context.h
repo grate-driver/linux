36eed0322e8d97bc6cacc82f131b27ae709bfa36
David Howells <dhowells@redhat.com>
2018-11-01 23:07:23 +0000

    vfs: Introduce additional defs for a new mount API
    Add additional definitions and members for a new mount API, including:
    
     (1) Two additional fs_context purposes for submounts and root mounts.
    
     (2) struct fs_parameter - Represents typed key=value parameter concept
         with which configuration data will be passed to filesystems.  This
         will allow for passing of more exotic values such as binary blobs,
         paths and fds with greater ease in addition to string values and null
         options.
    
     (3) In fs_context, an LSM data pointer.
    
     (4) In fs_context, a proposed s_fs_info value for a new superblock - if
         there's a new superblock (to be assigned automatically by the
         sget_fc() function when it appears).  This is provided separately from
         ->fs_private as the latter may hold things that the fs doesn't need to
         be stored in the object s_fs_info points to.  Providing s_fs_info
         allows that to be constructed in advance and just assigned over.
    
     (5) In fs_context, flags indicating whether "silent" or "sloppy" flags
         were passed.
    
     (6) struct fs_context_operations - A table of operations that is now
         pointed to by the fs_context.  These provide parameter wrangling hooks
         as well as action hooks for getting the desired root dentry or
         reconfiguring a superblock.
    
    Signed-off-by: David Howells <dhowells@redhat.com>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- include/linux/fs_context.h
+++ include/linux/fs_context.h
@@ -20,12 +20,48 @@ struct cred;
 struct dentry;
 struct file_operations;
 struct file_system_type;
+struct mnt_namespace;
 struct net;
+struct pid_namespace;
+struct super_block;
 struct user_namespace;
+struct vfsmount;
+struct path;
 
 enum fs_context_purpose {
 	FS_CONTEXT_FOR_MOUNT,		/* New superblock for explicit mount */
+	FS_CONTEXT_FOR_SUBMOUNT,	/* New superblock for automatic submount */
+	FS_CONTEXT_FOR_ROOT_MOUNT,	/* New superblock for internal root mount */
 	FS_CONTEXT_FOR_RECONFIGURE,	/* Superblock reconfiguration (remount) */
+};
+
+/*
+ * Type of parameter value.
+ */
+enum fs_value_type {
+	fs_value_is_undefined,
+	fs_value_is_flag,		/* Value not given a value */
+	fs_value_is_string,		/* Value is a string */
+	fs_value_is_blob,		/* Value is a binary blob */
+	fs_value_is_filename,		/* Value is a filename* + dirfd */
+	fs_value_is_filename_empty,	/* Value is a filename* + dirfd + AT_EMPTY_PATH */
+	fs_value_is_file,		/* Value is a file* */
+};
+
+/*
+ * Configuration parameter.
+ */
+struct fs_parameter {
+	const char		*key;		/* Parameter name */
+	enum fs_value_type	type:8;		/* The type of value here */
+	union {
+		char		*string;
+		void		*blob;
+		struct filename	*name;
+		struct file	*file;
+	};
+	size_t	size;
+	int	dirfd;
 };
 
 /*
@@ -38,6 +74,7 @@ enum fs_context_purpose {
  * See Documentation/filesystems/mounting.txt
  */
 struct fs_context {
+	const struct fs_context_operations *ops;
 	struct file_system_type	*fs_type;
 	void			*fs_private;	/* The filesystem's context */
 	struct dentry		*root;		/* The root and superblock */
@@ -47,10 +84,23 @@ struct fs_context {
 	const char		*source;	/* The source name (eg. dev path) */
 	const char		*subtype;	/* The subtype to set on the superblock */
 	void			*security;	/* Linux S&M options */
+	void			*s_fs_info;	/* Proposed s_fs_info */
 	unsigned int		sb_flags;	/* Proposed superblock flags (SB_*) */
 	unsigned int		sb_flags_mask;	/* Superblock flags that were changed */
 	enum fs_context_purpose	purpose:8;
+	bool			sloppy:1;	/* T if unrecognised options are okay */
+	bool			silent:1;	/* T if "o silent" specified */
 	bool			need_free:1;	/* Need to call ops->free() */
+};
+
+struct fs_context_operations {
+	void (*free)(struct fs_context *fc);
+	int (*dup)(struct fs_context *fc, struct fs_context *src_fc);
+	int (*parse_param)(struct fs_context *fc, struct fs_parameter *param);
+	int (*parse_monolithic)(struct fs_context *fc, void *data);
+	int (*validate)(struct fs_context *fc);
+	int (*get_tree)(struct fs_context *fc);
+	int (*reconfigure)(struct fs_context *fc);
 };
 
 /*
@@ -61,6 +111,7 @@ extern struct fs_context *vfs_new_fs_context(struct file_system_type *fs_type,
 					     unsigned int sb_flags,
 					     unsigned int sb_flags_mask,
 					     enum fs_context_purpose purpose);
+extern int generic_parse_monolithic(struct fs_context *fc, void *data);
 extern int vfs_get_tree(struct fs_context *fc);
 extern void put_fs_context(struct fs_context *fc);
 
