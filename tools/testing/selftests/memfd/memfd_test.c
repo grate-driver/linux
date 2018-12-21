e0b23d9cecb88719d66e9140b44065cae098609a
Joel Fernandes (Google) <joel@joelfernandes.org>
2018-12-22 10:55:28 +1100

    selftests/memfd: modify tests for F_SEAL_FUTURE_WRITE seal
    Modify the tests for F_SEAL_FUTURE_WRITE based on the changes
    introduced in previous patch.
    
    Also add a test to make sure the reopen issue pointed by Jann Horn [1]
    is fixed.
    
    [1] https://lore.kernel.org/lkml/CAG48ez1h=v-JYnDw81HaYJzOfrNhwYksxmc2r=cJvdQVgYM+NA@mail.gmail.com/
    
    Link: http://lkml.kernel.org/r/20181120052137.74317-2-joel@joelfernandes.org
    Signed-off-by: Joel Fernandes (Google) <joel@joelfernandes.org>
    Cc: Jann Horn <jannh@google.com>
    Cc: Al Viro <viro@zeniv.linux.org.uk>
    Cc: Andy Lutomirski <luto@kernel.org>
    Cc: Christoph Hellwig <hch@infradead.org>
    Cc: Daniel Colascione <dancol@google.com>
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Cc: J. Bruce Fields <bfields@fieldses.org>
    Cc: Jeff Layton <jlayton@kernel.org>
    Cc: John Reck <jreck@google.com>
    Cc: John Stultz <john.stultz@linaro.org>
    Cc: Khalid Aziz <khalid.aziz@oracle.com>
    Cc: Lei Yang <Lei.Yang@windriver.com>
    Cc: Marc-Andr Lureau <marcandre.lureau@redhat.com>
    Cc: Mike Kravetz <mike.kravetz@oracle.com>
    Cc: Minchan Kim <minchan@kernel.org>
    Cc: Shuah Khan <shuah@kernel.org>
    Cc: Todd Kjos <tkjos@google.com>
    Cc: Valdis Kletnieks <valdis.kletnieks@vt.edu>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>

--- tools/testing/selftests/memfd/memfd_test.c
+++ tools/testing/selftests/memfd/memfd_test.c
@@ -54,6 +54,22 @@ static int mfd_assert_new(const char *name, loff_t sz, unsigned int flags)
 	return fd;
 }
 
+static int mfd_assert_reopen_fd(int fd_in)
+{
+	int r, fd;
+	char path[100];
+
+	sprintf(path, "/proc/self/fd/%d", fd_in);
+
+	fd = open(path, O_RDWR);
+	if (fd < 0) {
+		printf("re-open of existing fd %d failed\n", fd_in);
+		abort();
+	}
+
+	return fd;
+}
+
 static void mfd_fail_new(const char *name, unsigned int flags)
 {
 	int r;
@@ -246,6 +262,25 @@ static void mfd_assert_read(int fd)
 		 mfd_def_size,
 		 PROT_READ | PROT_WRITE,
 		 MAP_PRIVATE,
+		 fd,
+		 0);
+	if (p == MAP_FAILED) {
+		printf("mmap() failed: %m\n");
+		abort();
+	}
+	munmap(p, mfd_def_size);
+}
+
+/* Test that PROT_READ + MAP_SHARED mappings work. */
+static void mfd_assert_read_shared(int fd)
+{
+	void *p;
+
+	/* verify PROT_READ and MAP_SHARED *is* allowed */
+	p = mmap(NULL,
+		 mfd_def_size,
+		 PROT_READ,
+		 MAP_SHARED,
 		 fd,
 		 0);
 	if (p == MAP_FAILED) {
@@ -698,7 +733,7 @@ static void test_seal_write(void)
  */
 static void test_seal_future_write(void)
 {
-	int fd;
+	int fd, fd2;
 	void *p;
 
 	printf("%s SEAL-FUTURE-WRITE\n", memfd_str);
@@ -710,58 +745,23 @@ static void test_seal_future_write(void)
 	p = mfd_assert_mmap_shared(fd);
 
 	mfd_assert_has_seals(fd, 0);
-	/* Not adding grow/shrink seals makes the future write
-	 * seal fail to get added
-	 */
-	mfd_fail_add_seals(fd, F_SEAL_FUTURE_WRITE);
 
-	mfd_assert_add_seals(fd, F_SEAL_GROW);
-	mfd_assert_has_seals(fd, F_SEAL_GROW);
-
-	/* Should still fail since shrink seal has
-	 * not yet been added
-	 */
-	mfd_fail_add_seals(fd, F_SEAL_FUTURE_WRITE);
-
-	mfd_assert_add_seals(fd, F_SEAL_SHRINK);
-	mfd_assert_has_seals(fd, F_SEAL_GROW |
-				 F_SEAL_SHRINK);
-
-	/* Now should succeed, also verifies that the seal
-	 * could be added with an existing writable mmap
-	 */
 	mfd_assert_add_seals(fd, F_SEAL_FUTURE_WRITE);
-	mfd_assert_has_seals(fd, F_SEAL_SHRINK |
-				 F_SEAL_GROW |
-				 F_SEAL_FUTURE_WRITE);
+	mfd_assert_has_seals(fd, F_SEAL_FUTURE_WRITE);
 
 	/* read should pass, writes should fail */
 	mfd_assert_read(fd);
+	mfd_assert_read_shared(fd);
 	mfd_fail_write(fd);
 
-	munmap(p, mfd_def_size);
-	close(fd);
-
-	/* Test adding all seals (grow, shrink, future write) at once */
-	fd = mfd_assert_new("kern_memfd_seal_future_write2",
-			    mfd_def_size,
-			    MFD_CLOEXEC | MFD_ALLOW_SEALING);
-
-	p = mfd_assert_mmap_shared(fd);
-
-	mfd_assert_has_seals(fd, 0);
-	mfd_assert_add_seals(fd, F_SEAL_SHRINK |
-				 F_SEAL_GROW |
-				 F_SEAL_FUTURE_WRITE);
-	mfd_assert_has_seals(fd, F_SEAL_SHRINK |
-				 F_SEAL_GROW |
-				 F_SEAL_FUTURE_WRITE);
-
-	/* read should pass, writes should fail */
-	mfd_assert_read(fd);
-	mfd_fail_write(fd);
+	fd2 = mfd_assert_reopen_fd(fd);
+	/* read should pass, writes should still fail */
+	mfd_assert_read(fd2);
+	mfd_assert_read_shared(fd2);
+	mfd_fail_write(fd2);
 
 	munmap(p, mfd_def_size);
+	close(fd2);
 	close(fd);
 }
 
