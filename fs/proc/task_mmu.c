ddf1c51b2b5dfa2c18f16d791095c53b9491e3c7
David Rientjes <rientjes@google.com>
2018-12-22 10:55:10 +1100

    mm, thp: always specify disabled vmas as nh in smaps
    1860033237d4 ("mm: make PR_SET_THP_DISABLE immediately active") introduced
    a regression in that userspace cannot always determine the set of vmas
    where thp is disabled.
    
    Userspace relies on the "nh" flag being emitted as part of /proc/pid/smaps
    to determine if a vma has been disabled from being backed by hugepages.
    
    Previous to this commit, prctl(PR_SET_THP_DISABLE, 1) would cause thp to
    be disabled and emit "nh" as a flag for the corresponding vmas as part of
    /proc/pid/smaps.  After the commit, thp is disabled by means of an mm flag
    and "nh" is not emitted.
    
    This causes smaps parsing libraries to assume a vma is enabled for thp and
    ends up puzzling the user on why its memory is not backed by thp.
    
    This also clears the "hg" flag to make the behavior of MADV_HUGEPAGE and
    PR_SET_THP_DISABLE definitive.
    
    Link: http://lkml.kernel.org/r/alpine.DEB.2.21.1809251449060.96762@chino.kir.corp.google.com
    Fixes: 1860033237d4 ("mm: make PR_SET_THP_DISABLE immediately active")
    Signed-off-by: David Rientjes <rientjes@google.com>
    Cc: Michal Hocko <mhocko@kernel.org>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Cc: Alexey Dobriyan <adobriyan@gmail.com>
    Cc: "Kirill A. Shutemov" <kirill.shutemov@linux.intel.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -653,13 +653,25 @@ static void show_smap_vma_flags(struct seq_file *m, struct vm_area_struct *vma)
 #endif
 #endif /* CONFIG_ARCH_HAS_PKEYS */
 	};
+	unsigned long flags = vma->vm_flags;
 	size_t i;
+
+	/*
+	 * Disabling thp is possible through both MADV_NOHUGEPAGE and
+	 * PR_SET_THP_DISABLE.  Both historically used VM_NOHUGEPAGE.  Since
+	 * the introduction of MMF_DISABLE_THP, however, userspace needs the
+	 * ability to detect vmas where thp is not eligible in the same manner.
+	 */
+	if (vma->vm_mm && test_bit(MMF_DISABLE_THP, &vma->vm_mm->flags)) {
+		flags &= ~VM_HUGEPAGE;
+		flags |= VM_NOHUGEPAGE;
+	}
 
 	seq_puts(m, "VmFlags: ");
 	for (i = 0; i < BITS_PER_LONG; i++) {
 		if (!mnemonics[i][0])
 			continue;
-		if (vma->vm_flags & (1UL << i)) {
+		if (flags & (1UL << i)) {
 			seq_putc(m, mnemonics[i][0]);
 			seq_putc(m, mnemonics[i][1]);
 			seq_putc(m, ' ');
