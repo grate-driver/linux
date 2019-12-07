31cd02b0be16deef631988eb7e5c621a76cd5572
Michał Mirosław <mirq-linux@rere.qmqm.pl>
2019-12-07 18:41:34 +0100

    input: Support Asus Transformer dock multimedia keys
    Some keys in Asus Dock report keycodes that don't make
    sense according to their position, this patch modifies the
    incoming data that is sent to serio to send proper scancodes.
    
    This enables normal keyboard keys to become available by default
    unless AltGr is pressed, which sends the multimedia keys sent
    originally. Pressing ScreenLock + AltGr switches the default
    choice between 2 type of keys.
    
    Since this only modifies codes sent by asus-ec keys ext it doesn't
    affect normal keyboards at all.
    
    Co-developed-by: Ion Agorria <ion@agorria.com>
    Signed-off-by: Ion Agorria <ion@agorria.com>
    Signed-off-by: Michał Mirosław <mirq-linux@rere.qmqm.pl>

--- drivers/input/serio/asusec-kbc.c
+++ drivers/input/serio/asusec-kbc.c
@@ -29,23 +29,57 @@ static int asusec_kbc_notify(struct notifier_block *nb,
 			     unsigned long action, void *data_)
 {
 	struct asusec_kbc_data *priv = container_of(nb, struct asusec_kbc_data, nb);
-	struct serio *port = NULL;
+	unsigned int port_idx = 0;
 	unsigned int n;
 	u8 *data = data_;
 
 	if (action & (ASUSEC_SMI_MASK | ASUSEC_SCI_MASK))
 		return NOTIFY_DONE;
 	else if (action & ASUSEC_AUX_MASK)
-		port = priv->sdev[1];
+		port_idx = 1;
 	else if (action & (ASUSEC_KBC_MASK | ASUSEC_KEY_MASK))
-		port = priv->sdev[0];
+		port_idx = 0;
 	else
 		return NOTIFY_DONE;
 
 	n = data[0] - 1;
 	data += 2;
+
+	/*
+	 * We need to replace these incoming data for keys:
+	 * RIGHT_META Press   0xE0 0x27      -> LEFT_ALT   Press   0x11
+	 * RIGHT_META Release 0xE0 0xF0 0x27 -> LEFT_ALT   Release 0xF0 0x11
+	 * COMPOSE    Press   0xE0 0x2F      -> RIGHT_META Press   0xE0 0x27
+	 * COMPOSE    Release 0xE0 0xF0 0x2F -> RIGHT_META Release 0xE0 0xF0 0x27
+	 */
+
+	if (port_idx == 0 && n >= 2 && data[0] == 0xE0) {
+		if (n == 3 && data[1] == 0xF0) {
+			switch (data[2]) {
+			case 0x27:
+				data[0] = 0xF0;
+				data[1] = 0x11;
+				n = 2;
+				break;
+			case 0x2F:
+				data[2] = 0x27;
+				break;
+			}
+		} else if (n == 2) {
+			switch (data[1]) {
+			case 0x27:
+				data[0] = 0x11;
+				n = 1;
+				break;
+			case 0x2F:
+				data[1] = 0x27;
+				break;
+			}
+		}
+	}
+
 	while (n--)
-		serio_interrupt(port, *data++, 0);
+		serio_interrupt(priv->sdev[port_idx], *data++, 0);
 
 	return NOTIFY_OK;
 }
