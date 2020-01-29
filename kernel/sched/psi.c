e92faa37b1551fe3d5b800786a8944eea83563a7
Stephen Rothwell <sfr@canb.auug.org.au>
2020-01-30 07:58:56 +1100

    proc-convert-everything-to-struct-proc_ops-fix-2
    fix kernel/sched/psi.c
    
    Link: http://lkml.kernel.org/r/20200122180545.36222f50@canb.auug.org.au
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>

--- kernel/sched/psi.c
+++ kernel/sched/psi.c
@@ -1283,8 +1283,8 @@ static int __init psi_proc_init(void)
 	if (psi_enable) {
 		proc_mkdir("pressure", NULL);
 		proc_create("pressure/io", 0, NULL, &psi_io_proc_ops);
-		proc_create("pressure/memory", 0, NUL, &psi_memory_proc_ops);
-		proc_create("pressure/cpu", 0, NULL &psi_cpu_proc_ops);
+		proc_create("pressure/memory", 0, NULL, &psi_memory_proc_ops);
+		proc_create("pressure/cpu", 0, NULL, &psi_cpu_proc_ops);
 	}
 	return 0;
 }
