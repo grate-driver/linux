e92c165b15bb23b2da32f37a1fdf84c01e53a7c9
David Howells <dhowells@redhat.com>
2020-02-06 14:22:29 +0000

    afs: Use new fscache read helper API
    Make AFS use the new fscache read helpers to implement the VM read
    operations:
    
     - afs_readpage() now hands off responsibility to fscache_readpage().
    
     - afs_readpages() is gone and replaced with afs_readahead().
    
     - afs_readahead() just hands off responsibility to fscache_readahead().
    
    These make use of the cache if a cookie is supplied, otherwise just call
    the ->issue_op() method a sufficient number of times to complete the entire
    request.
    
    Signed-off-by: David Howells <dhowells@redhat.com>

--- fs/afs/internal.h
+++ fs/afs/internal.h
@@ -207,6 +207,7 @@ struct afs_read {
 	loff_t			file_size;	/* File size returned by server */
 	struct key		*key;		/* The key to use to reissue the read */
 	struct afs_vnode	*vnode;		/* The file being read into. */
+	struct netfs_read_subrequest *subreq;	/* Fscache helper read request this belongs to */
 	afs_dataversion_t	data_version;	/* Version number returned by server */
 	refcount_t		usage;
 	unsigned int		call_debug_id;
@@ -1049,6 +1050,7 @@ extern void afs_put_wb_key(struct afs_wb_key *);
 extern int afs_open(struct inode *, struct file *);
 extern int afs_release(struct inode *, struct file *);
 extern int afs_fetch_data(struct afs_vnode *, struct afs_read *);
+extern struct afs_read *afs_alloc_read(gfp_t);
 extern void afs_put_read(struct afs_read *);
 
 static inline struct afs_read *afs_get_read(struct afs_read *req)
