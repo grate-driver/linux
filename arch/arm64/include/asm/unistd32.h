e91bed49f250773231c379aa01b89d70453aae08
David Howells <dhowells@redhat.com>
2020-02-21 15:30:56 +0000

    watch_queue: Implement mount topology and attribute change notifications
    Add a mount notification facility whereby notifications about changes in
    mount topology and configuration can be received.  Note that this only
    covers vfsmount topology changes and not superblock events.  A separate
    facility will be added for that.
    
    Every mount is given a change counter than counts the number of topological
    rearrangements in which it is involved and the number of attribute changes
    it undergoes.  This allows notification loss to be dealt with.  Later
    patches will provide a way to quickly retrieve this value, along with
    information about topology and parameters for the superblock.
    
    Firstly, a watch queue needs to be created:
    
    	pipe2(fds, O_NOTIFICATION_PIPE);
    	ioctl(fds[1], IOC_WATCH_QUEUE_SET_SIZE, 256);
    
    then a notification can be set up to report notifications via that queue:
    
    	struct watch_notification_filter filter = {
    		.nr_filters = 1,
    		.filters = {
    			[0] = {
    				.type = WATCH_TYPE_MOUNT_NOTIFY,
    				.subtype_filter[0] = UINT_MAX,
    			},
    		},
    	};
    	ioctl(fds[1], IOC_WATCH_QUEUE_SET_FILTER, &filter);
    	watch_mount(AT_FDCWD, "/", 0, fds[1], 0x02);
    
    In this case, it would let me monitor the mount topology subtree rooted at
    "/" for events.  Mount notifications propagate up the tree towards the
    root, so a watch will catch all of the events happening in the subtree
    rooted at the watch.
    
    After setting the watch, records will be placed into the queue when, for
    example, as superblock switches between read-write and read-only.  Records
    are of the following format:
    
    	struct mount_notification {
    		struct watch_notification watch;
    		__u32	triggered_on;
    		__u32	auxiliary_mount;
    		__u32	topology_changes;
    		__u32	attr_changes;
    		__u32	aux_topology_changes;
    	} *n;
    
    Where:
    
    	n->watch.type will be WATCH_TYPE_MOUNT_NOTIFY.
    
    	n->watch.subtype will indicate the type of event, such as
    	NOTIFY_MOUNT_NEW_MOUNT.
    
    	n->watch.info & WATCH_INFO_LENGTH will indicate the length of the
    	record.
    
    	n->watch.info & WATCH_INFO_ID will be the fifth argument to
    	watch_mount(), shifted.
    
    	n->watch.info & NOTIFY_MOUNT_IN_SUBTREE if true indicates that the
    	notifcation was generated in the mount subtree rooted at the watch,
    	and not actually in the watch itself.
    
    	n->watch.info & NOTIFY_MOUNT_IS_RECURSIVE if true indicates that
    	the notifcation was generated by an event (eg. SETATTR) that was
    	applied recursively.  The notification is only generated for the
    	object that initially triggered it.
    
    	n->watch.info & NOTIFY_MOUNT_IS_NOW_RO will be used for
    	NOTIFY_MOUNT_READONLY, being set if the superblock becomes R/O, and
    	being cleared otherwise, and for NOTIFY_MOUNT_NEW_MOUNT, being set
    	if the new mount is a submount (e.g. an automount).
    
    	n->watch.info & NOTIFY_MOUNT_IS_SUBMOUNT if true indicates that the
    	NOTIFY_MOUNT_NEW_MOUNT notification is in response to a mount
    	performed by the kernel (e.g. an automount).
    
    	n->triggered_on indicates the ID of the mount to which the change
    	was accounted (e.g. the new parent of a new mount).
    
    	n->axiliary_mount indicates the ID of an additional mount that was
    	affected (e.g. a new mount itself) or 0.
    
    	n->topology_changes provides the value of the topology change
    	counter of the triggered-on mount at the conclusion of the
    	operarion.
    
    	n->attr_changes provides the value of the attribute change counter
    	of the triggered-on mount at the conclusion of the operarion.
    
    	n->aux_topology_changes provides the value of the topology change
    	counter of the auxiliary mount at the conclusion of the operation.
    
    Note that it is permissible for event records to be of variable length -
    or, at least, the length may be dependent on the subtype.  Note also that
    the queue can be shared between multiple notifications of various types.
    
    Signed-off-by: David Howells <dhowells@redhat.com>

--- arch/arm64/include/asm/unistd32.h
+++ arch/arm64/include/asm/unistd32.h
@@ -883,6 +883,8 @@ __SYSCALL(__NR_clone3, sys_clone3)
 __SYSCALL(__NR_openat2, sys_openat2)
 #define __NR_pidfd_getfd 438
 __SYSCALL(__NR_pidfd_getfd, sys_pidfd_getfd)
+#define __NR_watch_mount 439
+__SYSCALL(__NR_watch_mount, sys_watch_mount)
 
 /*
  * Please add new compat syscalls above this comment and update
