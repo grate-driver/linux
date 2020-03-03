a7c3f6a618e074549c20eecb6872d11098b1f628
David Howells <dhowells@redhat.com>
2020-03-03 16:42:02 +0000

    fsinfo: Add a uniquifier ID to struct mount
    Add a uniquifier ID to struct mount that is effectively unique over the
    kernel lifetime to deal around mnt_id values being reused.  This can then
    be exported through fsinfo() to allow detection of replacement mounts that
    happen to end up with the same mount ID.
    
    The normal mount handle is still used for referring to a particular mount.
    
    Signed-off-by: David Howells <dhowells@redhat.com>

--- fs/namespace.c
+++ fs/namespace.c
@@ -115,6 +115,9 @@ static int mnt_alloc_id(struct mount *mnt)
 	if (res < 0)
 		return res;
 	mnt->mnt_id = res;
+#ifdef CONFIG_FSINFO
+	vfs_generate_unique_id(&mnt->mnt_unique_id);
+#endif
 	return 0;
 }
 
