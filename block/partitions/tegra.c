bcf7d1ca2719812bd457d9524f41fe8dddf2ca61
Dmitry Osipenko <digetx@gmail.com>
2020-05-14 11:48:57 +0300

    partitions/tegra: Support enforced GPT scanning
    Downstream NVIDIA bootloader provides gpt_sector=<sector> kernel command
    line option to the kernel. This option should instruct the GPT partition
    parser to look at the specified sector for a valid GPT header if the GPT
    is not found at the beginning or the end of a block device. Support of
    this feature is needed by Tegra-based devices that have TegraPT and GPT
    placed in inaccessible by kernel locations.  The GPT entry duplicates
    TegraPT partitions.
    
    Secondly, some Tegra-based devices have bootloader that enforces the GPT
    scanning of the backup/alternative GPT entry by providing "gpt" cmdline
    option to the kernel, but doesn't provide the "gpt_sector" option.
    In this case GPT entry resides at a special offset from the end of eMMC
    storage.  It is a common situation for older bootloader versions.
    
    The offset is calculated as a total number of eMMC sectors minus number of
    eMMC boot sectors minus 1.  This equation is explicitly defined and used
    by the downstream Tegra kernels for locating GPT entry.
    
    Signed-off-by: Dmitry Osipenko <digetx@gmail.com>

--- block/partitions/tegra.c
+++ block/partitions/tegra.c
@@ -575,3 +575,60 @@ int tegra_partition(struct parsed_partitions *state)
 
 	return ret;
 }
+
+/*
+ * This allows a kernel command line option 'gpt_sector=<sector>' to
+ * enable GPT header lookup at a non-standard location. This option
+ * is provided to kernel by NVIDIA's proprietary bootloader.
+ */
+static sector_t tegra_gpt_sector;
+static int __init tegra_gpt_sector_fn(char *str)
+{
+	WARN_ON(kstrtoull(str, 10, &tegra_gpt_sector) < 0);
+	return 1;
+}
+__setup("gpt_sector=", tegra_gpt_sector_fn);
+
+int tegra_partition_forced_gpt(struct parsed_partitions *state)
+{
+	int ret = 0;
+
+#ifdef CONFIG_EFI_PARTITION
+	struct tegra_partition_table_parser ptp = {};
+
+	if (!soc_is_tegra())
+		return 0;
+
+	ptp.state = state;
+
+	ptp.boot_offset = tegra_partition_table_emmc_boot_offset(&ptp);
+	if (ptp.boot_offset < 0)
+		return 0;
+
+	/*
+	 * Some Tegra devices do not use gpt_sector=<sector> kernel command
+	 * line option. In this case these devices usually have a GPT entry
+	 * at the end of the block device and the GPT entry address is
+	 * calculated this way for eMMC:
+	 *
+	 * gpt_sector = ext_csd.sectors_num - ext_csd.boot_sectors_num - 1
+	 *
+	 * This algorithm is defined and used by NVIDIA in the downstream
+	 * kernel of those devices.
+	 *
+	 * Please note that bootloader supplies the "gpt" cmdline option
+	 * which enforces the GPT scanning, meaning that the scanning will
+	 * be a NO-OP on devices that do not use GPT.
+	 */
+	if (tegra_gpt_sector) {
+		state->force_gpt_sector = tegra_gpt_sector;
+	} else {
+		state->force_gpt_sector  = get_capacity(state->bdev->bd_disk);
+		state->force_gpt_sector -= ptp.boot_offset + 1;
+	}
+
+	ret = efi_partition(state);
+	state->force_gpt_sector = 0;
+#endif
+	return ret;
+}
