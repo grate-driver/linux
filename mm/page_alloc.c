cbf7c9d86d757e2f2aa575c7084917a21aae2002
Daniel Vetter <daniel.vetter@ffwll.ch>
2020-06-21 15:20:32 +1000

    mm: track mmu notifiers in fs_reclaim_acquire/release
    fs_reclaim_acquire/release nicely catch recursion issues when allocating
    GFP_KERNEL memory against shrinkers (which gpu drivers tend to use to keep
    the excessive caches in check).  For mmu notifier recursions we do have
    lockdep annotations since 23b68395c7c7 ("mm/mmu_notifiers: add a lockdep
    map for invalidate_range_start/end").
    
    But these only fire if a path actually results in some pte invalidation -
    for most small allocations that's very rarely the case.  The other trouble
    is that pte invalidation can happen any time when __GFP_RECLAIM is set.
    Which means only really GFP_ATOMIC is a safe choice, GFP_NOIO isn't good
    enough to avoid potential mmu notifier recursion.
    
    I was pondering whether we should just do the general annotation, but
    there's always the risk for false positives.  Plus I'm assuming that the
    core fs and io code is a lot better reviewed and tested than random mmu
    notifier code in drivers.  Hence why I decide to only annotate for that
    specific case.
    
    Furthermore even if we'd create a lockdep map for direct reclaim, we'd
    still need to explicit pull in the mmu notifier map - there's a lot more
    places that do pte invalidation than just direct reclaim, these two
    contexts arent the same.
    
    Note that the mmu notifiers needing their own independent lockdep map is
    also the reason we can't hold them from fs_reclaim_acquire to
    fs_reclaim_release - it would nest with the acquistion in the pte
    invalidation code, causing a lockdep splat.  And we can't remove the
    annotations from pte invalidation and all the other places since they're
    called from many other places than page reclaim.  Hence we can only do the
    equivalent of might_lock, but on the raw lockdep map.
    
    With this we can also remove the lockdep priming added in 66204f1d2d1b
    ("mm/mmu_notifiers: prime lockdep") since the new annotations are strictly
    more powerful.
    
    Link: http://lkml.kernel.org/r/20200610194101.1668038-1-daniel.vetter@ffwll.ch
    Signed-off-by: Daniel Vetter <daniel.vetter@intel.com>
    Acked-by: Jason Gunthorpe <jgg@mellanox.com>	[mmu_notifiers]
    Cc: Thomas Hellström (Intel) <thomas_os@shipmail.org>
    Cc: Maarten Lankhorst <maarten.lankhorst@linux.intel.com>
    Cc: Christian König <christian.koenig@amd.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -57,6 +57,7 @@
 #include <trace/events/oom.h>
 #include <linux/prefetch.h>
 #include <linux/mm_inline.h>
+#include <linux/mmu_notifier.h>
 #include <linux/migrate.h>
 #include <linux/hugetlb.h>
 #include <linux/sched/rt.h>
@@ -4155,7 +4156,7 @@ should_compact_retry(struct alloc_context *ac, unsigned int order, int alloc_fla
 static struct lockdep_map __fs_reclaim_map =
 	STATIC_LOCKDEP_MAP_INIT("fs_reclaim", &__fs_reclaim_map);
 
-static bool __need_fs_reclaim(gfp_t gfp_mask)
+static bool __need_reclaim(gfp_t gfp_mask)
 {
 	gfp_mask = current_gfp_context(gfp_mask);
 
@@ -4165,10 +4166,6 @@ static bool __need_fs_reclaim(gfp_t gfp_mask)
 
 	/* this guy won't enter reclaim */
 	if (current->flags & PF_MEMALLOC)
-		return false;
-
-	/* We're only interested __GFP_FS allocations for now */
-	if (!(gfp_mask & __GFP_FS))
 		return false;
 
 	if (gfp_mask & __GFP_NOLOCKDEP)
@@ -4189,15 +4186,25 @@ void __fs_reclaim_release(void)
 
 void fs_reclaim_acquire(gfp_t gfp_mask)
 {
-	if (__need_fs_reclaim(gfp_mask))
-		__fs_reclaim_acquire();
+	if (__need_reclaim(gfp_mask)) {
+		if (gfp_mask & __GFP_FS)
+			__fs_reclaim_acquire();
+
+#ifdef CONFIG_MMU_NOTIFIER
+		lock_map_acquire(&__mmu_notifier_invalidate_range_start_map);
+		lock_map_release(&__mmu_notifier_invalidate_range_start_map);
+#endif
+
+	}
 }
 EXPORT_SYMBOL_GPL(fs_reclaim_acquire);
 
 void fs_reclaim_release(gfp_t gfp_mask)
 {
-	if (__need_fs_reclaim(gfp_mask))
-		__fs_reclaim_release();
+	if (__need_reclaim(gfp_mask)) {
+		if (gfp_mask & __GFP_FS)
+			__fs_reclaim_release();
+	}
 }
 EXPORT_SYMBOL_GPL(fs_reclaim_release);
 #endif
