4617515818834ba5dd4dd4fcac5258be9fe8a57d
Waiman Long <longman@redhat.com>
2020-06-21 15:20:36 +1000

    include/linux/sched/mm.h: optimize current_gfp_context()
    The current_gfp_context() converts a number of PF_MEMALLOC_* per-process
    flags into the corresponding GFP_* flags for memory allocation.  In that
    function, current->flags is accessed 3 times.  That may lead to duplicated
    access of the same memory location.
    
    This is not usually a problem with minimal debug config options on as the
    compiler can optimize away the duplicated memory accesses.  With most of
    the debug config options on, however, that may not be the case.  For
    example, the x86-64 object size of the __need_fs_reclaim() in a debug
    kernel that calls current_gfp_context() was 309 bytes.  With this patch
    applied, the object size is reduced to 202 bytes.  This is a saving of 107
    bytes and will probably be slightly faster too.
    
    Link: http://lkml.kernel.org/r/20200618212936.9776-1-longman@redhat.com
    Signed-off-by: Waiman Long <longman@redhat.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Cc: Michel Lespinasse <walken@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>

--- include/linux/sched/mm.h
+++ include/linux/sched/mm.h
@@ -181,18 +181,20 @@ static inline bool in_vfork(struct task_struct *tsk)
  */
 static inline gfp_t current_gfp_context(gfp_t flags)
 {
-	if (unlikely(current->flags &
+	unsigned int pflags = READ_ONCE(current->flags);
+
+	if (unlikely(pflags &
 		     (PF_MEMALLOC_NOIO | PF_MEMALLOC_NOFS | PF_MEMALLOC_NOCMA))) {
 		/*
 		 * NOIO implies both NOIO and NOFS and it is a weaker context
 		 * so always make sure it makes precedence
 		 */
-		if (current->flags & PF_MEMALLOC_NOIO)
+		if (pflags & PF_MEMALLOC_NOIO)
 			flags &= ~(__GFP_IO | __GFP_FS);
-		else if (current->flags & PF_MEMALLOC_NOFS)
+		else if (pflags & PF_MEMALLOC_NOFS)
 			flags &= ~__GFP_FS;
 #ifdef CONFIG_CMA
-		if (current->flags & PF_MEMALLOC_NOCMA)
+		if (pflags & PF_MEMALLOC_NOCMA)
 			flags &= ~__GFP_MOVABLE;
 #endif
 	}
