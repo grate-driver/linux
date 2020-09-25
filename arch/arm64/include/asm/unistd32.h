49fab14b6886eede14026bb3c24ac71cd5cce858
Stephen Rothwell <sfr@canb.auug.org.au>
2020-09-25 14:52:12 +1000

    mm-madvise-introduce-process_madvise-syscall-an-external-memory-hinting-api-fix-fix-fix
    Link: https://lkml.kernel.org/r/20200905142639.49fc3f1a@canb.auug.org.au
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Cc: Minchan Kim <minchan@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>

--- arch/arm64/include/asm/unistd32.h
+++ arch/arm64/include/asm/unistd32.h
@@ -889,7 +889,7 @@ __SYSCALL(__NR_pidfd_getfd, sys_pidfd_getfd)
 __SYSCALL(__NR_faccessat2, sys_faccessat2)
 #define __NR_watch_mount 440
 __SYSCALL(__NR_watch_mount, sys_watch_mount)
-#define __NR_watch_mount 441
+#define __NR_process_madvise 441
 __SYSCALL(__NR_process_madvise, sys_process_madvise)
 
 /*
