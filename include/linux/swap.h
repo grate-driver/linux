929a61ae6e3e802f8bc06bdb319dc5b300906519
Matthew Wilcox (Oracle) <willy@infradead.org>
2020-09-25 14:50:01 +1000

    mm: factor find_get_incore_page out of mincore_page
    Patch series "Return head pages from find_*_entry", v2.
    
    This patch series started out as part of the THP patch set, but it has
    some nice effects along the way and it seems worth splitting it out and
    submitting separately.
    
    Currently find_get_entry() and find_lock_entry() return the page
    corresponding to the requested index, but the first thing most callers do
    is find the head page, which we just threw away.  As part of auditing all
    the callers, I found some misuses of the APIs and some plain
    inefficiencies that I've fixed.
    
    The diffstat is unflattering, but I added more kernel-doc and a new wrapper.
    
    This patch (of 8);
    
    Provide this functionality from the swap cache.  It's useful for
    more than just mincore().
    
    Link: https://lkml.kernel.org/r/20200910183318.20139-1-willy@infradead.org
    Link: https://lkml.kernel.org/r/20200910183318.20139-2-willy@infradead.org
    Signed-off-by: Matthew Wilcox (Oracle) <willy@infradead.org>
    Cc: Hugh Dickins <hughd@google.com>
    Cc: William Kucharski <william.kucharski@oracle.com>
    Cc: Jani Nikula <jani.nikula@linux.intel.com>
    Cc: Alexey Dobriyan <adobriyan@gmail.com>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Matthew Auld <matthew.auld@intel.com>
    Cc: Huang Ying <ying.huang@intel.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>

--- include/linux/swap.h
+++ include/linux/swap.h
@@ -427,6 +427,7 @@ extern void free_pages_and_swap_cache(struct page **, int);
 extern struct page *lookup_swap_cache(swp_entry_t entry,
 				      struct vm_area_struct *vma,
 				      unsigned long addr);
+struct page *find_get_incore_page(struct address_space *mapping, pgoff_t index);
 extern struct page *read_swap_cache_async(swp_entry_t, gfp_t,
 			struct vm_area_struct *vma, unsigned long addr,
 			bool do_poll);
@@ -567,6 +568,12 @@ static inline struct page *lookup_swap_cache(swp_entry_t swp,
 					     unsigned long addr)
 {
 	return NULL;
+}
+
+static inline
+struct page *find_get_incore_page(struct address_space *mapping, pgoff_t index)
+{
+	return find_get_page(mapping, index);
 }
 
 static inline int add_to_swap(struct page *page)
