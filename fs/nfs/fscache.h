bc6d7b12e4ea88dd63517d0f207dbbb79a2f16ac
Dave Wysochanski <dwysocha@redhat.com>
2020-11-14 18:21:40 -0500

    NFS: Convert to the netfs API and nfs_readpage to use netfs_readpage
    This patch converts the main NFS read paths to the new netfs API,
    when fscache is enabled, and converts readpage while minimizing
    changes to the existing NFS read code paths.
    
    The netfs API requires a few functions to be provided by the
    netfs:
    - init_rreq: allows netfs to allocate resources prior to IO
    - is_cache_enabled: allows netfs to disable fscache
    - begin_cache_operation: signals the start of an fscache IO
    - issue_op: called when netfs should issue read to server
    - clamp_length: allows netfs to limit size of IO
    - cleanup: allows netfs to cleanup after an IO is complete
    
    The new netfs_readpage() API is called when fscache is enabled.
    If a read cannot be satisfied from fscache, the netfs is called
    back via issue_op() to obtain the data from the server.  Once
    the read completes, the netfs must call netfs_subreq_terminated()
    which then may write the data to fscache.  In order to call back
    into fscache via netfs_subreq_terminated(), we must save the
    netfs_read_subrequest* as a field in the nfs_pgio_header, similar
    to nfs_direct_req.
    
    If the netfs has a read IO limit (for example, NFS 'rsize' mount
    options) the clamp_length() function is called.
    
    Signed-off-by: Dave Wysochanski <dwysocha@redhat.com>

--- fs/nfs/fscache.h
+++ fs/nfs/fscache.h
@@ -95,13 +95,14 @@ extern void nfs_fscache_open_file(struct inode *, struct file *);
 
 extern void __nfs_fscache_invalidate_page(struct page *, struct inode *);
 extern int nfs_fscache_release_page(struct page *, gfp_t);
-
-extern int __nfs_readpage_from_fscache(struct nfs_open_context *,
-				       struct inode *, struct page *);
+extern int nfs_readpage_from_fscache(struct file *file,
+				     struct page *page,
+				     struct nfs_readdesc *desc);
 extern int __nfs_readpages_from_fscache(struct nfs_open_context *,
 					struct inode *, struct address_space *,
 					struct list_head *, unsigned *);
-extern void __nfs_readpage_to_fscache(struct inode *, struct page *, int);
+extern void nfs_read_completion_to_fscache(struct nfs_pgio_header *hdr,
+					   unsigned long bytes);
 
 /*
  * wait for a page to complete writing to the cache
@@ -125,18 +126,6 @@ static inline void nfs_fscache_invalidate_page(struct page *page,
 }
 
 /*
- * Retrieve a page from an inode data storage object.
- */
-static inline int nfs_readpage_from_fscache(struct nfs_open_context *ctx,
-					    struct inode *inode,
-					    struct page *page)
-{
-	if (NFS_I(inode)->fscache)
-		return __nfs_readpage_from_fscache(ctx, inode, page);
-	return -ENOBUFS;
-}
-
-/*
  * Retrieve a set of pages from an inode data storage object.
  */
 static inline int nfs_readpages_from_fscache(struct nfs_open_context *ctx,
@@ -149,18 +138,6 @@ static inline int nfs_readpages_from_fscache(struct nfs_open_context *ctx,
 		return __nfs_readpages_from_fscache(ctx, inode, mapping, pages,
 						    nr_pages);
 	return -ENOBUFS;
-}
-
-/*
- * Store a page newly fetched from the server in an inode data storage object
- * in the cache.
- */
-static inline void nfs_readpage_to_fscache(struct inode *inode,
-					   struct page *page,
-					   int sync)
-{
-	if (PageFsCache(page))
-		__nfs_readpage_to_fscache(inode, page, sync);
 }
 
 /*
@@ -212,9 +189,9 @@ static inline void nfs_fscache_invalidate_page(struct page *page,
 static inline void nfs_fscache_wait_on_page_write(struct nfs_inode *nfsi,
 						  struct page *page) {}
 
-static inline int nfs_readpage_from_fscache(struct nfs_open_context *ctx,
-					    struct inode *inode,
-					    struct page *page)
+static inline int nfs_readpage_from_fscache(struct file *file,
+					    struct page *page,
+					    struct nfs_readdesc *desc)
 {
 	return -ENOBUFS;
 }
@@ -226,9 +203,8 @@ static inline int nfs_readpages_from_fscache(struct nfs_open_context *ctx,
 {
 	return -ENOBUFS;
 }
-static inline void nfs_readpage_to_fscache(struct inode *inode,
-					   struct page *page, int sync) {}
-
+static inline void nfs_read_completion_to_fscache(struct nfs_pgio_header *hdr,
+						  unsigned long bytes) {}
 
 static inline void nfs_fscache_invalidate(struct inode *inode) {}
 static inline void nfs_fscache_wait_on_invalidate(struct inode *inode) {}
