76581c3c179d1c43cbec7490d159a481484e8ee8
Dmitry Osipenko <digetx@gmail.com>
2020-11-20 15:32:54 +0300

    soc/tegra: Introduce core power domain driver
    NVIDIA Tegra SoCs have multiple power domains, each domain corresponds
    to an external SoC power rail. Core power domain covers vast majority of
    hardware blocks within a Tegra SoC. The voltage of a power domain should
    be set to a value which satisfies all devices within a power domain. Add
    driver for the core power domain in order to manage the voltage state of
    the domain. This allows us to support a system-wide DVFS on Tegra.
    
    Tested-by: Peter Geis <pgwipeout@gmail.com>
    Tested-by: Nicolas Chauvet <kwizart@gmail.com>
    Tested-by: Matt Merhar <mattmerhar@protonmail.com>
    Signed-off-by: Dmitry Osipenko <digetx@gmail.com>

--- include/soc/tegra/common.h
+++ include/soc/tegra/common.h
@@ -22,10 +22,16 @@ struct tegra_core_opp_params {
 
 #ifdef CONFIG_ARCH_TEGRA
 bool soc_is_tegra(void);
+bool tegra_soc_core_domain_state_synced(void);
 int devm_tegra_core_dev_init_opp_table(struct device *dev,
 				       struct tegra_core_opp_params *params);
 #else
 static inline bool soc_is_tegra(void)
+{
+	return false;
+}
+
+static inline bool tegra_soc_core_domain_state_synced(void)
 {
 	return false;
 }
