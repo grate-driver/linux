f726c72d6c059c1a6db425108cd6c220fb95cbf4
Barry Song <song.bao.hua@hisilicon.com>
2020-12-12 23:18:44 +1300

    dma-mapping: benchmark: check the validity of dma mask bits
    While dma_mask_bits is larger than 64, the bahvaiour is undefined. On the
    other hand, dma_mask_bits which is smaller than 20 (1MB) makes no sense
    in real hardware.
    
    Reported-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Barry Song <song.bao.hua@hisilicon.com>
    Signed-off-by: Christoph Hellwig <hch@lst.de>

--- kernel/dma/map_benchmark.c
+++ kernel/dma/map_benchmark.c
@@ -214,6 +214,12 @@ static long map_benchmark_ioctl(struct file *file, unsigned int cmd,
 			return -EINVAL;
 		}
 
+		if (map->bparam.dma_bits < 20 ||
+		    map->bparam.dma_bits > 64) {
+			pr_err("invalid dma_bits\n");
+			return -EINVAL;
+		}
+
 		if (map->bparam.node != NUMA_NO_NODE &&
 		    !node_possible(map->bparam.node)) {
 			pr_err("invalid numa node\n");
