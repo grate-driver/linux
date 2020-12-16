8c941e9a8a137497d68d926c51e9b100c996de99
Dmitry Osipenko <digetx@gmail.com>
2020-12-16 23:05:50 +0300

    XXX: usb: phy: tegra: Ensure that clock is disabled on suspend
    Ensure that clock is disabled on suspend to catch LPM programming bugs.

--- drivers/usb/phy/phy-tegra-usb.c
+++ drivers/usb/phy/phy-tegra-usb.c
@@ -330,6 +330,8 @@ static int utmip_pad_power_on(struct tegra_usb_phy *phy)
 	u32 val;
 	int err;
 
+	cancel_delayed_work_sync(&phy->clk_dbg_check_work);
+
 	err = clk_prepare_enable(phy->pad_clk);
 	if (err)
 		return err;
@@ -402,6 +404,13 @@ ulock:
 	spin_unlock(&utmip_pad_lock);
 
 	clk_disable_unprepare(phy->pad_clk);
+
+	cancel_delayed_work_sync(&phy->clk_dbg_check_work);
+	if (phy->wakeup_enabled) {
+		schedule_delayed_work(&phy->clk_dbg_check_work, 0);
+		flush_delayed_work(&phy->clk_dbg_check_work);
+		schedule_delayed_work(&phy->clk_dbg_check_work, 15 * HZ);
+	}
 
 	return ret;
 }
@@ -1337,6 +1346,25 @@ static const struct of_device_id tegra_usb_phy_id_table[] = {
 };
 MODULE_DEVICE_TABLE(of, tegra_usb_phy_id_table);
 
+static void clk_dbg_check_work(struct work_struct *work)
+{
+	struct tegra_usb_phy *phy = container_of(work, struct tegra_usb_phy,
+						 clk_dbg_check_work.work);
+
+	dev_info(phy->u_phy.dev,
+		"debug: USB_PHY_VBUS_SENSORS 0x%08x\n",
+		readl_relaxed(phy->regs + USB_PHY_VBUS_SENSORS));
+
+	dev_info(phy->u_phy.dev,
+		"debug: USB_PHY_VBUS_WAKEUP_ID 0x%08x\n",
+		readl_relaxed(phy->regs + USB_PHY_VBUS_WAKEUP_ID));
+
+	if (readl_relaxed(phy->regs + USB_SUSP_CTRL) & USB_PHY_CLK_VALID)
+		dev_info(phy->u_phy.dev, "debug: clk active after suspend\n");
+	else
+		dev_info(phy->u_phy.dev, "debug: suspend ok\n");
+}
+
 static int tegra_usb_phy_probe(struct platform_device *pdev)
 {
 	struct device_node *np = pdev->dev.of_node;
@@ -1476,6 +1504,8 @@ static int tegra_usb_phy_probe(struct platform_device *pdev)
 	tegra_phy->u_phy.set_suspend = tegra_usb_phy_set_suspend;
 
 	platform_set_drvdata(pdev, tegra_phy);
+
+	INIT_DELAYED_WORK(&tegra_phy->clk_dbg_check_work, clk_dbg_check_work);
 
 	return usb_add_phy_dev(&tegra_phy->u_phy);
 }
