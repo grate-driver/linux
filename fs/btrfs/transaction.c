79df1a9901b821fe0ab3a390301b236691569953
Josef Bacik <josef@toxicpanda.com>
2020-12-16 11:26:30 -0500

    btrfs: handle record_root_in_trans failure in qgroup_account_snapshot
    record_root_in_trans can fail currently, so handle this failure
    properly.
    
    Reviewed-by: Qu Wenruo <wqu@suse.com>
    Signed-off-by: Josef Bacik <josef@toxicpanda.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/transaction.c
+++ fs/btrfs/transaction.c
@@ -1445,7 +1445,9 @@ static int qgroup_account_snapshot(struct btrfs_trans_handle *trans,
 	 * recorded root will never be updated again, causing an outdated root
 	 * item.
 	 */
-	record_root_in_trans(trans, src, 1);
+	ret = record_root_in_trans(trans, src, 1);
+	if (ret)
+		return ret;
 
 	/*
 	 * btrfs_qgroup_inherit relies on a consistent view of the usage for the
@@ -1514,7 +1516,7 @@ out:
 	 * insert_dir_item()
 	 */
 	if (!ret)
-		record_root_in_trans(trans, parent, 1);
+		ret = record_root_in_trans(trans, parent, 1);
 	return ret;
 }
 
