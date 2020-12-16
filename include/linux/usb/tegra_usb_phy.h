f628a60e44ad2aeec2d9923b93996604901e8ffd
Dmitry Osipenko <digetx@gmail.com>
2020-12-16 23:05:50 +0300

    XXX: usb: phy: tegra: Ensure that clock is disabled on suspend
    Ensure that clock is disabled on suspend to catch LPM programming bugs.

--- include/linux/usb/tegra_usb_phy.h
+++ include/linux/usb/tegra_usb_phy.h
@@ -21,6 +21,7 @@
 #include <linux/regmap.h>
 #include <linux/reset.h>
 #include <linux/usb/otg.h>
+#include <linux/workqueue.h>
 
 /*
  * utmi_pll_config_in_car_module: true if the UTMI PLL configuration registers
@@ -87,6 +88,8 @@ struct tegra_usb_phy {
 	bool wakeup_enabled;
 	bool pad_wakeup;
 	bool powered_on;
+
+	struct delayed_work clk_dbg_check_work;
 };
 
 void tegra_usb_phy_preresume(struct usb_phy *phy);
