74dff4e4d5f13307374c31066a5e3973ca904fa6
Dmitry Osipenko <digetx@gmail.com>
2020-12-21 00:57:22 +0300

    WIP: drm/grate: Add Host1x and DRM drivers with experimental changes
    The new drivers and UAPI provide all features that are necessary for
    implementing of a proper userspace driver. The new driver takes a different
    approach in regards to working with sync points by making each submitted job
    to take an individual sync point, this solves the problem with the sync point
    recovery that old driver suffered from. The new Host1x driver and v2 UAPI are
    optimized for performance and minimal resources consumption. The new v2 UAPI
    exposes to userspace the HW cmdstream-related features that are necessary
    for a proper HW fencing. The staging v1 UAPI is kept functional and all of
    current userspace will continue to work with the new driver, note that some
    of the never-really-used features of v1 UAPI have been removed (like sync
    point increment IOCTL) and will return EPERM.
    
    The goal is to merge new features into the main driver once they are ready.
    
    Key moments of v2 UAPI:
    	- Raw sync points are not exposed to userspace.
    
    	- Job descriptors are embedded into the commands stream. Kernel
    	  driver parses the stream and patches the descriptors in-place.
    
    	- Commands buffer is copied from userspace via usrptr (no BO
    	  allocation and uncached-reading overhead).
    
    	- Channel is not restricted to a single client, thus 3d channel can
    	  take a multi-client job that uses 2d / 3d.
    
    	- Supports explicit jobs fencing. DRM sync object and DRM scheduler
    	  are utilized to provide the fencing support.
    
    	- Allows to use host1x gather opcode, which is restricted to
    	  data-upload usecase only. This allows userspace to pre-allocate
    	  a gather buffer, put shaders code / data there, and use two-word
    	  "gather" opcode to upload data from the buffer without pushing
    	  that data into the commands stream on each submission.
    
    Signed-off-by: Dmitry Osipenko <digetx@gmail.com>

---
+++ drivers/gpu/host1x-grate/soc/hw/hw_host1x04_channel.h
@@ -0,0 +1,135 @@
+/*
+ * Copyright (c) 2013 NVIDIA Corporation.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+ /*
+  * Function naming determines intended use:
+  *
+  *     <x>_r(void) : Returns the offset for register <x>.
+  *
+  *     <x>_w(void) : Returns the word offset for word (4 byte) element <x>.
+  *
+  *     <x>_<y>_s(void) : Returns size of field <y> of register <x> in bits.
+  *
+  *     <x>_<y>_f(u32 v) : Returns a value based on 'v' which has been shifted
+  *         and masked to place it at field <y> of register <x>.  This value
+  *         can be |'d with others to produce a full register value for
+  *         register <x>.
+  *
+  *     <x>_<y>_m(void) : Returns a mask for field <y> of register <x>.  This
+  *         value can be ~'d and then &'d to clear the value of field <y> for
+  *         register <x>.
+  *
+  *     <x>_<y>_<z>_f(void) : Returns the constant value <z> after being shifted
+  *         to place it at field <y> of register <x>.  This value can be |'d
+  *         with others to produce a full register value for <x>.
+  *
+  *     <x>_<y>_v(u32 r) : Returns the value of field <y> from a full register
+  *         <x> value 'r' after being shifted to place its LSB at bit 0.
+  *         This value is suitable for direct comparison with other unshifted
+  *         values appropriate for use in field <y> of register <x>.
+  *
+  *     <x>_<y>_<z>_v(void) : Returns the constant value for <z> defined for
+  *         field <y> of register <x>.  This value is suitable for direct
+  *         comparison with unshifted values appropriate for use in field <y>
+  *         of register <x>.
+  */
+
+#ifndef HOST1X_HW_HOST1X04_CHANNEL_H
+#define HOST1X_HW_HOST1X04_CHANNEL_H
+
+#define CHANNEL_IO_SIZE		0x4000
+
+static inline u32 host1x_channel_fifostat_r(unsigned int id)
+{
+	return 0x0 + id * CHANNEL_IO_SIZE;
+}
+#define HOST1X_CHANNEL_FIFOSTAT(id) \
+	host1x_channel_fifostat_r(id)
+static inline u32 host1x_channel_fifostat_cfempty_v(u32 r)
+{
+	return (r >> 10) & 0x1;
+}
+#define HOST1X_CHANNEL_FIFOSTAT_CFEMPTY_V(r) \
+	host1x_channel_fifostat_cfempty_v(r)
+static inline u32 host1x_channel_dmastart_r(unsigned int id)
+{
+	return 0x14 + id * CHANNEL_IO_SIZE;
+}
+#define HOST1X_CHANNEL_DMASTART(id) \
+	host1x_channel_dmastart_r(id)
+static inline u32 host1x_channel_dmaput_r(unsigned int id)
+{
+	return 0x18 + id * CHANNEL_IO_SIZE;
+}
+#define HOST1X_CHANNEL_DMAPUT(id) \
+	host1x_channel_dmaput_r(id)
+static inline u32 host1x_channel_dmaget_r(unsigned int id)
+{
+	return 0x1c + id * CHANNEL_IO_SIZE;
+}
+#define HOST1X_CHANNEL_DMAGET(id) \
+	host1x_channel_dmaget_r(id)
+static inline u32 host1x_channel_dmaend_r(unsigned int id)
+{
+	return 0x20 + id * CHANNEL_IO_SIZE;
+}
+#define HOST1X_CHANNEL_DMAEND(id) \
+	host1x_channel_dmaend_r(id)
+static inline u32 host1x_channel_dmactrl_r(unsigned int id)
+{
+	return 0x24 + id * CHANNEL_IO_SIZE;
+}
+#define HOST1X_CHANNEL_DMACTRL(id) \
+	host1x_channel_dmactrl_r(id)
+static inline u32 host1x_channel_dmactrl_dmastop(void)
+{
+	return 1 << 0;
+}
+#define HOST1X_CHANNEL_DMACTRL_DMASTOP \
+	host1x_channel_dmactrl_dmastop()
+static inline u32 host1x_channel_dmactrl_dmastop_v(u32 r)
+{
+	return (r >> 0) & 0x1;
+}
+#define HOST1X_CHANNEL_DMACTRL_DMASTOP_V(r) \
+	host1x_channel_dmactrl_dmastop_v(r)
+static inline u32 host1x_channel_dmactrl_dmagetrst(void)
+{
+	return 1 << 1;
+}
+#define HOST1X_CHANNEL_DMACTRL_DMAGETRST \
+	host1x_channel_dmactrl_dmagetrst()
+static inline u32 host1x_channel_dmactrl_dmainitget(void)
+{
+	return 1 << 2;
+}
+#define HOST1X_CHANNEL_DMACTRL_DMAINITGET \
+	host1x_channel_dmactrl_dmainitget()
+static inline u32 host1x_channel_channelctrl_r(unsigned int id)
+{
+	return 0x98 + id * CHANNEL_IO_SIZE;
+}
+#define HOST1X_CHANNEL_CHANNELCTRL(id) \
+	host1x_channel_channelctrl_r(id)
+static inline u32 host1x_channel_channelctrl_kernel_filter_gbuffer_f(u32 v)
+{
+	return (v & 0x1) << 2;
+}
+#define HOST1X_CHANNEL_CHANNELCTRL_KERNEL_FILTER_GBUFFER(v) \
+	host1x_channel_channelctrl_kernel_filter_gbuffer_f(v)
+
+#endif
