66a1b1f5828b58baa1982ad01cfc1af5dd758257
Yangtao Li <tiny.windzz@gmail.com>
2021-01-01 16:54:40 +0000

    opp: Add devres wrapper for dev_pm_opp_of_add_table
    Add devres wrapper for dev_pm_opp_of_add_table() to simplify driver
    code.
    
    Signed-off-by: Yangtao Li <tiny.windzz@gmail.com>

--- include/linux/pm_opp.h
+++ include/linux/pm_opp.h
@@ -438,6 +438,7 @@ int dev_pm_opp_of_add_table(struct device *dev);
 int dev_pm_opp_of_add_table_indexed(struct device *dev, int index);
 int dev_pm_opp_of_add_table_noclk(struct device *dev, int index);
 void dev_pm_opp_of_remove_table(struct device *dev);
+int devm_pm_opp_of_add_table(struct device *dev);
 int dev_pm_opp_of_cpumask_add_table(const struct cpumask *cpumask);
 void dev_pm_opp_of_cpumask_remove_table(const struct cpumask *cpumask);
 int dev_pm_opp_of_get_sharing_cpus(struct device *cpu_dev, struct cpumask *cpumask);
@@ -468,6 +469,11 @@ static inline int dev_pm_opp_of_add_table_noclk(struct device *dev, int index)
 
 static inline void dev_pm_opp_of_remove_table(struct device *dev)
 {
+}
+
+static inline int devm_pm_opp_of_add_table(struct device *dev)
+{
+	return -ENOTSUPP;
 }
 
 static inline int dev_pm_opp_of_cpumask_add_table(const struct cpumask *cpumask)
