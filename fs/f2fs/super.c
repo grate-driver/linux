2d84e621175be305e16ae659385c7eb692665387
Daeho Jeong <daehojeong@google.com>
2021-01-19 09:00:42 +0900

    f2fs: introduce checkpoint=merge mount option
    We've added a new mount option "checkpoint=merge", which creates a
    kernel daemon and makes it to merge concurrent checkpoint requests as
    much as possible to eliminate redundant checkpoint issues. Plus, we
    can eliminate the sluggish issue caused by slow checkpoint operation
    when the checkpoint is done in a process context in a cgroup having
    low i/o budget and cpu shares. To make this do better, we set the
    default i/o priority of the kernel daemon to "3", to give one higher
    priority than other kernel threads. The below verification result
    explains this.
    The basic idea has come from https://opensource.samsung.com.
    
    [Verification]
    Android Pixel Device(ARM64, 7GB RAM, 256GB UFS)
    Create two I/O cgroups (fg w/ weight 100, bg w/ wight 20)
    Set "strict_guarantees" to "1" in BFQ tunables
    
    In "fg" cgroup,
    - thread A => trigger 1000 checkpoint operations
      "for i in `seq 1 1000`; do touch test_dir1/file; fsync test_dir1;
       done"
    - thread B => gererating async. I/O
      "fio --rw=write --numjobs=1 --bs=128k --runtime=3600 --time_based=1
           --filename=test_img --name=test"
    
    In "bg" cgroup,
    - thread C => trigger repeated checkpoint operations
      "echo $$ > /dev/blkio/bg/tasks; while true; do touch test_dir2/file;
       fsync test_dir2; done"
    
    We've measured thread A's execution time.
    
    [ w/o patch ]
    Elapsed Time: Avg. 68 seconds
    [ w/  patch ]
    Elapsed Time: Avg. 48 seconds
    
    Reported-by: kernel test robot <lkp@intel.com>
    Reported-by: Dan Carpenter <dan.carpenter@oracle.com>
    [Jaegeuk Kim: fix the return value in f2fs_start_ckpt_thread, reported by Dan]
    Signed-off-by: Daeho Jeong <daehojeong@google.com>
    Signed-off-by: Sungjong Seo <sj1557.seo@samsung.com>
    Reviewed-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/super.c
+++ fs/f2fs/super.c
@@ -144,6 +144,7 @@ enum {
 	Opt_checkpoint_disable_cap,
 	Opt_checkpoint_disable_cap_perc,
 	Opt_checkpoint_enable,
+	Opt_checkpoint_merge,
 	Opt_compress_algorithm,
 	Opt_compress_log_size,
 	Opt_compress_extension,
@@ -214,6 +215,7 @@ static match_table_t f2fs_tokens = {
 	{Opt_checkpoint_disable_cap, "checkpoint=disable:%u"},
 	{Opt_checkpoint_disable_cap_perc, "checkpoint=disable:%u%%"},
 	{Opt_checkpoint_enable, "checkpoint=enable"},
+	{Opt_checkpoint_merge, "checkpoint=merge"},
 	{Opt_compress_algorithm, "compress_algorithm=%s"},
 	{Opt_compress_log_size, "compress_log_size=%u"},
 	{Opt_compress_extension, "compress_extension=%s"},
@@ -923,6 +925,9 @@ static int parse_options(struct super_block *sb, char *options, bool is_remount)
 		case Opt_checkpoint_enable:
 			clear_opt(sbi, DISABLE_CHECKPOINT);
 			break;
+		case Opt_checkpoint_merge:
+			set_opt(sbi, MERGE_CHECKPOINT);
+			break;
 #ifdef CONFIG_F2FS_FS_COMPRESSION
 		case Opt_compress_algorithm:
 			if (!f2fs_sb_has_compression(sbi)) {
@@ -1116,6 +1121,12 @@ static int parse_options(struct super_block *sb, char *options, bool is_remount)
 
 	if (test_opt(sbi, DISABLE_CHECKPOINT) && f2fs_lfs_mode(sbi)) {
 		f2fs_err(sbi, "LFS not compatible with checkpoint=disable\n");
+		return -EINVAL;
+	}
+
+	if (test_opt(sbi, DISABLE_CHECKPOINT) &&
+			test_opt(sbi, MERGE_CHECKPOINT)) {
+		f2fs_err(sbi, "checkpoint=merge cannot be used with checkpoint=disable\n");
 		return -EINVAL;
 	}
 
@@ -1323,6 +1334,12 @@ static void f2fs_put_super(struct super_block *sb)
 	mutex_lock(&sbi->umount_mutex);
 
 	/*
+	 * flush all issued checkpoints and stop checkpoint issue thread.
+	 * after then, all checkpoints should be done by each process context.
+	 */
+	f2fs_stop_ckpt_thread(sbi);
+
+	/*
 	 * We don't need to do checkpoint when superblock is clean.
 	 * But, the previous checkpoint was not done by umount, it needs to do
 	 * clean checkpoint again.
@@ -1420,15 +1437,9 @@ int f2fs_sync_fs(struct super_block *sb, int sync)
 	if (unlikely(is_sbi_flag_set(sbi, SBI_POR_DOING)))
 		return -EAGAIN;
 
-	if (sync) {
-		struct cp_control cpc;
+	if (sync)
+		err = f2fs_issue_checkpoint(sbi);
 
-		cpc.reason = __get_cp_reason(sbi);
-
-		down_write(&sbi->gc_lock);
-		err = f2fs_write_checkpoint(sbi, &cpc);
-		up_write(&sbi->gc_lock);
-	}
 	return err;
 }
 
@@ -1752,6 +1763,8 @@ static int f2fs_show_options(struct seq_file *seq, struct dentry *root)
 	if (test_opt(sbi, DISABLE_CHECKPOINT))
 		seq_printf(seq, ",checkpoint=disable:%u",
 				F2FS_OPTION(sbi).unusable_cap);
+	if (test_opt(sbi, MERGE_CHECKPOINT))
+		seq_puts(seq, ",checkpoint=merge");
 	if (F2FS_OPTION(sbi).fsync_mode == FSYNC_MODE_POSIX)
 		seq_printf(seq, ",fsync_mode=%s", "posix");
 	else if (F2FS_OPTION(sbi).fsync_mode == FSYNC_MODE_STRICT)
@@ -2029,6 +2042,18 @@ static int f2fs_remount(struct super_block *sb, int *flags, char *data)
 				goto restore_gc;
 		} else {
 			f2fs_enable_checkpoint(sbi);
+		}
+	}
+
+	if (!test_opt(sbi, MERGE_CHECKPOINT)) {
+		f2fs_stop_ckpt_thread(sbi);
+	} else {
+		err = f2fs_start_ckpt_thread(sbi);
+		if (err) {
+			f2fs_err(sbi,
+			    "Failed to start F2FS issue_checkpoint_thread (%d)",
+			    err);
+			goto restore_gc;
 		}
 	}
 
@@ -3783,6 +3808,18 @@ try_onemore:
 
 	f2fs_init_fsync_node_info(sbi);
 
+	/* setup checkpoint request control and start checkpoint issue thread */
+	f2fs_init_ckpt_req_control(sbi);
+	if (test_opt(sbi, MERGE_CHECKPOINT)) {
+		err = f2fs_start_ckpt_thread(sbi);
+		if (err) {
+			f2fs_err(sbi,
+			    "Failed to start F2FS issue_checkpoint_thread (%d)",
+			    err);
+			goto stop_ckpt_thread;
+		}
+	}
+
 	/* setup f2fs internal modules */
 	err = f2fs_build_segment_manager(sbi);
 	if (err) {
@@ -3992,6 +4029,8 @@ free_nm:
 free_sm:
 	f2fs_destroy_segment_manager(sbi);
 	f2fs_destroy_post_read_wq(sbi);
+stop_ckpt_thread:
+	f2fs_stop_ckpt_thread(sbi);
 free_devices:
 	destroy_device_list(sbi);
 	kvfree(sbi->ckpt);
