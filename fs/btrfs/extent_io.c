45da9eb9d534f8712b27aa75cf355eb88631b95b
Naohiro Aota <naohiro.aota@wdc.com>
2021-01-26 11:24:53 +0900

    btrfs: redirty released extent buffers in ZONED mode
    Tree manipulating operations like merging nodes often release
    once-allocated tree nodes. Btrfs cleans such nodes so that pages in the
    node are not uselessly written out. On ZONED volumes, however, such
    optimization blocks the following IOs as the cancellation of the write out
    of the freed blocks breaks the sequential write sequence expected by the
    device.
    
    This patch introduces a list of clean and unwritten extent buffers that
    have been released in a transaction. Btrfs redirty the buffer so that
    btree_write_cache_pages() can send proper bios to the devices.
    
    Besides it clears the entire content of the extent buffer not to confuse
    raw block scanners e.g. btrfsck. By clearing the content,
    csum_dirty_buffer() complains about bytenr mismatch, so avoid the checking
    and checksum using newly introduced buffer flag EXTENT_BUFFER_NO_CHECK.
    
    Reviewed-by: Josef Bacik <josef@toxicpanda.com>
    Signed-off-by: Naohiro Aota <naohiro.aota@wdc.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/extent_io.c
+++ fs/btrfs/extent_io.c
@@ -24,6 +24,7 @@
 #include "rcu-string.h"
 #include "backref.h"
 #include "disk-io.h"
+#include "zoned.h"
 
 static struct kmem_cache *extent_state_cache;
 static struct kmem_cache *extent_buffer_cache;
@@ -5043,6 +5044,7 @@ __alloc_extent_buffer(struct btrfs_fs_info *fs_info, u64 start,
 
 	btrfs_leak_debug_add(&fs_info->eb_leak_lock, &eb->leak_list,
 			     &fs_info->allocated_ebs);
+	INIT_LIST_HEAD(&eb->release_list);
 
 	spin_lock_init(&eb->refs_lock);
 	atomic_set(&eb->refs, 1);
@@ -5829,6 +5831,8 @@ void write_extent_buffer(const struct extent_buffer *eb, const void *srcv,
 	char *kaddr;
 	char *src = (char *)srcv;
 	unsigned long i = get_eb_page_index(start);
+
+	WARN_ON(test_bit(EXTENT_BUFFER_NO_CHECK, &eb->bflags));
 
 	if (check_eb_range(eb, start, len))
 		return;
