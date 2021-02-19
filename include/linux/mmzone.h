8b3f16cc30bfee4a149659ddb76718c113783f20
David Hildenbrand <david@redhat.com>
2021-02-19 13:35:35 +1100

    mm-page_alloc-count-cma-pages-per-zone-and-print-them-in-proc-zoneinfo-v2
    - Print/track only with CONFIG_CMA
    - Extend patch description
    
    Link: https://lkml.kernel.org/r/20210128164533.18566-1-david@redhat.com
    Signed-off-by: David Hildenbrand <david@redhat.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: "Peter Zijlstra (Intel)" <peterz@infradead.org>
    Cc: Mike Rapoport <rppt@kernel.org>
    Cc: Oscar Salvador <osalvador@suse.de>
    Cc: Michal Hocko <mhocko@kernel.org>
    Cc: Wei Yang <richard.weiyang@linux.alibaba.com>
    Cc: David Rientjes <rientjes@google.com>
    Cc: Zi Yan <ziy@nvidia.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>

--- include/linux/mmzone.h
+++ include/linux/mmzone.h
@@ -530,7 +530,9 @@ struct zone {
 	atomic_long_t		managed_pages;
 	unsigned long		spanned_pages;
 	unsigned long		present_pages;
+#ifdef CONFIG_CMA
 	unsigned long		cma_pages;
+#endif
 
 	const char		*name;
 
