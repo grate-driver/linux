c5b5eea2860e80eb2934157eddf08b9398eed4c7
Jiapeng Chong <jiapeng.chong@linux.alibaba.com>
2021-05-14 10:39:57 +1000

    sysctl: remove redundant assignment to first
    Variable first is set to '0', but this value is never read as it is not
    used later on, hence it is a redundant assignment and can be removed.
    
    Clean up the following clang-analyzer warning:
    
    kernel/sysctl.c:1562:4: warning: Value stored to 'first' is never read
    [clang-analyzer-deadcode.DeadStores].
    
    Link: https://lkml.kernel.org/r/1620469990-22182-1-git-send-email-jiapeng.chong@linux.alibaba.com
    Signed-off-by: Jiapeng Chong <jiapeng.chong@linux.alibaba.com>
    Reported-by: Abaci Robot <abaci@linux.alibaba.com>
    Acked-by: Luis Chamberlain <mcgrof@kernel.org>
    Cc: Kees Cook <keescook@chromium.org>
    Cc: Iurii Zaikin <yzaikin@google.com>
    Cc: Alexei Starovoitov <ast@kernel.org>
    Cc: Daniel Borkmann <daniel@iogearbox.net>
    Cc: Andrii Nakryiko <andrii@kernel.org>
    Cc: Martin KaFai Lau <kafai@fb.com>
    Cc: Song Liu <songliubraving@fb.com>
    Cc: Yonghong Song <yhs@fb.com>
    Cc: John Fastabend <john.fastabend@gmail.com>
    Cc: KP Singh <kpsingh@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -1474,7 +1474,6 @@ int proc_do_large_bitmap(struct ctl_table *table, int write,
 			 void *buffer, size_t *lenp, loff_t *ppos)
 {
 	int err = 0;
-	bool first = 1;
 	size_t left = *lenp;
 	unsigned long bitmap_len = table->maxlen;
 	unsigned long *bitmap = *(unsigned long **) table->data;
@@ -1559,12 +1558,12 @@ int proc_do_large_bitmap(struct ctl_table *table, int write,
 			}
 
 			bitmap_set(tmp_bitmap, val_a, val_b - val_a + 1);
-			first = 0;
 			proc_skip_char(&p, &left, '\n');
 		}
 		left += skipped;
 	} else {
 		unsigned long bit_a, bit_b = 0;
+		bool first = 1;
 
 		while (left) {
 			bit_a = find_next_bit(bitmap, bitmap_len, bit_b);
