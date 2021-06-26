8700132dedca9ef407eff62c3c40cdfcc6b76e26
Dmitry Osipenko <digetx@gmail.com>
2021-06-26 19:45:48 +0300

    soc/tegra: pmc: Expose USB regmap to all SoCs
    All Tegra SoCs prior to Tegra186 have USB power controls within the Power
    Management controller. These controls need to be configured by USB driver.
    Expose the regmap to these SoCs.
    
    Signed-off-by: Dmitry Osipenko <digetx@gmail.com>

--- drivers/soc/tegra/pmc.c
+++ drivers/soc/tegra/pmc.c
@@ -3167,7 +3167,7 @@ static const struct tegra_pmc_soc tegra20_pmc_soc = {
 	.pmc_clks_data = NULL,
 	.num_pmc_clks = 0,
 	.has_blink_output = true,
-	.has_usb_sleepwalk = false,
+	.has_usb_sleepwalk = true,
 };
 
 static const char * const tegra30_powergates[] = {
@@ -3228,7 +3228,7 @@ static const struct tegra_pmc_soc tegra30_pmc_soc = {
 	.pmc_clks_data = tegra_pmc_clks_data,
 	.num_pmc_clks = ARRAY_SIZE(tegra_pmc_clks_data),
 	.has_blink_output = true,
-	.has_usb_sleepwalk = false,
+	.has_usb_sleepwalk = true,
 };
 
 static const char * const tegra114_powergates[] = {
@@ -3285,7 +3285,7 @@ static const struct tegra_pmc_soc tegra114_pmc_soc = {
 	.pmc_clks_data = tegra_pmc_clks_data,
 	.num_pmc_clks = ARRAY_SIZE(tegra_pmc_clks_data),
 	.has_blink_output = true,
-	.has_usb_sleepwalk = false,
+	.has_usb_sleepwalk = true,
 };
 
 static const char * const tegra124_powergates[] = {
