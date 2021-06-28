f49d469e7442dcba425b04402419466c19403589
Svyatoslav Ryhel <clamor95@gmail.com>
2021-06-28 09:12:14 +0300

    Revert "power/supply: asus-ec: Correct scaling of reported values"
    This reverts commit 9ee62b90b94aa81234a15dfd7521fb4ededca890.

--- drivers/power/supply/asusec-battery.c
+++ drivers/power/supply/asusec-battery.c
@@ -139,8 +139,7 @@ static int pad_battery_get_property(struct power_supply *psy,
 
 		val->intval = (s16)ret;
 
-		switch (psp) {
-		case POWER_SUPPLY_PROP_STATUS:
+		if (psp == POWER_SUPPLY_PROP_STATUS) {
 			if (ret & ASUSEC_BATTERY_FULL_CHARGED)
 				val->intval = POWER_SUPPLY_STATUS_FULL;
 			else if (ret & ASUSEC_BATTERY_FULL_DISCHARGED)
@@ -149,21 +148,8 @@ static int pad_battery_get_property(struct power_supply *psy,
 				val->intval = POWER_SUPPLY_STATUS_DISCHARGING;
 			else
 				val->intval = POWER_SUPPLY_STATUS_CHARGING;
-			break;
-
-		case POWER_SUPPLY_PROP_TEMP:
+		} else if (psp == POWER_SUPPLY_PROP_TEMP) {
 			val->intval -= TEMP_CELSIUS_OFFSET;
-			break;
-
-		case POWER_SUPPLY_PROP_CHARGE_NOW:
-		case POWER_SUPPLY_PROP_CURRENT_NOW:
-		case POWER_SUPPLY_PROP_VOLTAGE_NOW:
-		case POWER_SUPPLY_PROP_VOLTAGE_MAX:
-			val->intval *= 1000;
-			break;
-
-		default:
-			break;
 		}
 
 		break;
