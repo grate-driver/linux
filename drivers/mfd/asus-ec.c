7f9de47e63aff98c46631013abe5046e5c3e6956
Svyatoslav Ryhel <clamor95@gmail.com>
2021-06-28 09:14:19 +0300

    Revert "mfd: asus-ec: add TF101 and TF701T support"
    This reverts commit 4ae325f3bc755bb159a6b83660a2aa76b801ede2.

--- drivers/mfd/asus-ec.c
+++ drivers/mfd/asus-ec.c
@@ -108,29 +108,17 @@ static const struct mfd_cell asus_ec_subdev[] =
 };
 
 static const struct asus_ec_initdata asus_ec_model_info[] = {
-	{	/* Asus T20 Mobile Dock  */
-		.model		= "ASUS-EP101-DOCK",
-		.name		= "dock",
-		.components	= EC_PART_BATTERY|EC_PART_CHARGE_LED|
-				  EC_PART_I8042|EC_PART_EXT_KEYS,
-	},
-	{	/* Asus T30 Transformer Pad */
+	{	/* Asus Transformer Pad */
 		.model		= "ASUS-TF201-PAD",
 		.name		= "pad",
 		.components	= EC_PART_BATTERY|EC_PART_CHARGE_LED,
 		.flags		= EC_FLAG_SET_MODE,
 	},
-	{	/* Asus T30 Mobile Dock */
+	{	/* Asus Mobile Dock */
 		.model		= "ASUS-TF201-DOCK",
 		.name		= "dock",
 		.components	= EC_PART_BATTERY|EC_PART_CHARGE_LED|
 				  EC_PART_I8042|EC_PART_EXT_KEYS,
-	},
-	{	/* Asus T114 Transformer Pad */
-		.model		= "ASUS-TF701T-PAD",
-		.name		= "pad",
-		.components	= EC_PART_BATTERY|EC_PART_CHARGE_LED,
-		.flags		= EC_FLAG_SET_MODE,
 	},
 };
 
