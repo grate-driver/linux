022c6db72a828d4a5cfbe8e069436afe668a24b0
jenneron <jenneron@protonmail.com>
2021-07-03 02:37:03 +0300

    WIP: use Surface RT compatible instead of config for parking protocol

--- arch/arm/include/asm/acpi_parking_protocol.h
+++ arch/arm/include/asm/acpi_parking_protocol.h
@@ -7,7 +7,7 @@ extern const struct smp_operations acpi_parking_protocol_ops;
 
 static inline bool acpi_parking_protocol_available(void)
 {
-	return IS_ENABLED(CONFIG_ARM_ACPI_PARKING_PROTOCOL);
+	return of_machine_is_compatible("microsoft,surface-rt-efi");
 }
 
 #endif /* __ASM_ARM_ACPI_PARKING_PROTOCOL_H */
