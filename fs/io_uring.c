412e387cacfc3a93cd895d1b9f8441a68b7cb5fa
Pavel Begunkov <asml.silence@gmail.com>
2021-08-19 12:56:25 +0100

    io_uring: remove mutex in io_req_task_cancel()
    io_req_complete_failed() only does io_req_complete_post(), which we
    don't need to protect by ->uring_lock, so remove mutex_lock / unlock
    from there.
    
    Signed-off-by: Pavel Begunkov <asml.silence@gmail.com>
    Link: https://lore.kernel.org/r/4479bb41699ed5d868c7f9a423c2dfb4d68541de.1629374041.git.asml.silence@gmail.com
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- fs/io_uring.c
+++ fs/io_uring.c
@@ -2096,12 +2096,7 @@ static void io_req_task_work_add(struct io_kiocb *req)
 
 static void io_req_task_cancel(struct io_kiocb *req)
 {
-	struct io_ring_ctx *ctx = req->ctx;
-
-	/* ctx is guaranteed to stay alive while we hold uring_lock */
-	mutex_lock(&ctx->uring_lock);
 	io_req_complete_failed(req, req->result);
-	mutex_unlock(&ctx->uring_lock);
 }
 
 static void io_req_task_submit(struct io_kiocb *req)
