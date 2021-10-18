35a393d4ecac2cc1d004169eb1fb8743ef3378ef
Dmitry Osipenko <digetx@gmail.com>
2021-10-18 20:47:48 +0300

    mfd: ab8500: Use devm_register_simple_power_off_handler()
    Use devm_register_simple_power_off_handler() that replaces global
    pm_power_off variable and allows to register multiple power-off handlers.
    
    Signed-off-by: Dmitry Osipenko <digetx@gmail.com>

--- drivers/mfd/ab8500-sysctrl.c
+++ drivers/mfd/ab8500-sysctrl.c
@@ -26,7 +26,7 @@
 
 static struct device *sysctrl_dev;
 
-static void ab8500_power_off(void)
+static void ab8500_power_off(void *data)
 {
 	sigset_t old;
 	sigset_t all;
@@ -134,20 +134,8 @@ static int ab8500_sysctrl_probe(struct platform_device *pdev)
 {
 	sysctrl_dev = &pdev->dev;
 
-	if (!pm_power_off)
-		pm_power_off = ab8500_power_off;
-
-	return 0;
-}
-
-static int ab8500_sysctrl_remove(struct platform_device *pdev)
-{
-	sysctrl_dev = NULL;
-
-	if (pm_power_off == ab8500_power_off)
-		pm_power_off = NULL;
-
-	return 0;
+	return devm_register_simple_power_off_handler(sysctrl_dev,
+						      ab8500_power_off, NULL);
 }
 
 static const struct of_device_id ab8500_sysctrl_match[] = {
@@ -161,7 +149,6 @@ static struct platform_driver ab8500_sysctrl_driver = {
 		.of_match_table = ab8500_sysctrl_match,
 	},
 	.probe = ab8500_sysctrl_probe,
-	.remove = ab8500_sysctrl_remove,
 };
 
 static int __init ab8500_sysctrl_init(void)
