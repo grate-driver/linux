d33a236a834c45ee00ca63a6d2d7fb5feceb0e9e
Dmitry Osipenko <digetx@gmail.com>
2021-11-02 14:41:24 +0300

    ARM: orion5x: Use register_platform_power_off()
    Utilize new register_platform_power_off() API that allows to register
    multiple power-off handlers and deprecates global pm_power_off callback.
    
    Signed-off-by: Dmitry Osipenko <digetx@gmail.com>

--- arch/arm/mach-orion5x/ts409-setup.c
+++ arch/arm/mach-orion5x/ts409-setup.c
@@ -12,6 +12,7 @@
 #include <linux/init.h>
 #include <linux/platform_device.h>
 #include <linux/pci.h>
+#include <linux/reboot.h>
 #include <linux/irq.h>
 #include <linux/mtd/physmap.h>
 #include <linux/mv643xx_eth.h>
@@ -303,7 +304,7 @@ static void __init qnap_ts409_init(void)
 	platform_device_register(&ts409_leds);
 
 	/* register tsx09 specific power-off method */
-	pm_power_off = qnap_tsx09_power_off;
+	register_platform_power_off(qnap_tsx09_power_off);
 }
 
 MACHINE_START(TS409, "QNAP TS-409")
