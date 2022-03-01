cdb1ac81d1049928e9ec94382060dfa222d0a56d
Jason A. Donenfeld <Jason@zx2c4.com>
2022-03-01 15:14:04 +0100

    random: do not export add_vmfork_randomness() unless needed
    Since add_vmfork_randomness() is only called from vmgenid.o, we can
    guard it in CONFIG_VMGENID, similarly to how we do with
    add_disk_randomness() and CONFIG_BLOCK. If we ever have multiple things
    calling into add_vmfork_randomness(), we can add another shared Kconfig
    symbol for that, but for now, this is good enough. Even though
    add_vmfork_randomess() is a pretty small function, removing it means
    that there are only calls to crng_reseed(false) and none to
    crng_reseed(true), which means the compiler can constant propagate the
    false, removing branches from crng_reseed() and its descendants.
    
    Additionally, we don't even need the symbol to be exported if
    CONFIG_VMGENID is not a module, so conditionalize that too.
    
    Cc: Dominik Brodowski <linux@dominikbrodowski.net>
    Cc: Theodore Ts'o <tytso@mit.edu>
    Signed-off-by: Jason A. Donenfeld <Jason@zx2c4.com>

--- drivers/char/random.c
+++ drivers/char/random.c
@@ -1169,6 +1169,7 @@ void add_bootloader_randomness(const void *buf, size_t size)
 }
 EXPORT_SYMBOL_GPL(add_bootloader_randomness);
 
+#if IS_ENABLED(CONFIG_VMGENID)
 /*
  * Handle a new unique VM ID, which is unique, not secret, so we
  * don't credit it, but we do immediately force a reseed after so
@@ -1182,7 +1183,10 @@ void add_vmfork_randomness(const void *unique_vm_id, size_t size)
 		pr_notice("crng reseeded due to virtual machine fork\n");
 	}
 }
+#if IS_MODULE(CONFIG_VMGENID)
 EXPORT_SYMBOL_GPL(add_vmfork_randomness);
+#endif
+#endif
 
 struct fast_pool {
 	union {
