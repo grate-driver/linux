198f4ab91238be566ea96f687e49e830d6bd4bff
Maxim Schwalm <maxim.schwalm@gmail.com>
2022-05-17 20:19:42 +0200

    FIXME: Bluetooth: bcm: Add BCM43241B0 support
    Signed-off-by: Maxim Schwalm <maxim.schwalm@gmail.com>

--- drivers/bluetooth/hci_bcm.c
+++ drivers/bluetooth/hci_bcm.c
@@ -1537,6 +1537,7 @@ static struct bcm_device_data bcm43438_device_data = {
 
 static const struct of_device_id bcm_bluetooth_of_match[] = {
 	{ .compatible = "brcm,bcm20702a1" },
+	{ .compatible = "brcm,bcm43241b0-bt" },
 	{ .compatible = "brcm,bcm4329-bt" },
 	{ .compatible = "brcm,bcm4330-bt" },
 	{ .compatible = "brcm,bcm4334-bt" },
