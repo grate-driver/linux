01f8f07b52c4af3d3ecff9fa24affba8a6065cd5
Maxim Schwalm <maxim.schwalm@gmail.com>
2022-08-01 18:27:57 +0200

    usb: misc: onboard_hub: Add support for Genesys Logic USB2.0 Hub
    Add support for the Genesys Logic USB2.0 Hub found on the Asus P1801-T.
    
    Signed-off-by: Maxim Schwalm <maxim.schwalm@gmail.com>

--- drivers/usb/misc/onboard_usb_hub.h
+++ drivers/usb/misc/onboard_usb_hub.h
@@ -23,6 +23,7 @@ static const struct onboard_hub_pdata ti_tusb8041_data = {
 };
 
 static const struct of_device_id onboard_hub_match[] = {
+	{ .compatible = "usb5e3,610", .data = &realtek_rts5411_data, },
 	{ .compatible = "usb424,2514", .data = &microchip_usb424_data, },
 	{ .compatible = "usb451,8140", .data = &ti_tusb8041_data, },
 	{ .compatible = "usb451,8142", .data = &ti_tusb8041_data, },
