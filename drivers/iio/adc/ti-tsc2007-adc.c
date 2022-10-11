950e601da1f835170dd7321443063ca92c2501d3
Svyatoslav Ryhel <clamor95@gmail.com>
2022-10-11 19:36:14 +0300

    drivers: iio: tsc2007: add lg x3 specific stuff
    LG used this adc sensor for battery temperature
    monitoring. Unfortunately it is too device
    specific so I have made separate dts compatible
    to implement this.
    
    Signed-off-by: Svyatoslav Ryhel <clamor95@gmail.com>

--- drivers/iio/adc/ti-tsc2007-adc.c
+++ drivers/iio/adc/ti-tsc2007-adc.c
@@ -8,8 +8,9 @@
 #include <linux/slab.h>
 #include <linux/input.h>
 #include <linux/i2c.h>
+#include <linux/of.h>
+#include <linux/of_device.h>
 #include <linux/iio/iio.h>
-#include <linux/platform_data/tsc2007.h>
 
 #define TSC2007_MEASURE_TEMP0		(0x0 << 4)
 #define TSC2007_MEASURE_AUX		(0x2 << 4)
@@ -34,17 +35,43 @@
 #define READ_X		(ADC_ON_12BIT | TSC2007_MEASURE_X)
 #define PWRDOWN		(TSC2007_12BIT | TSC2007_POWER_OFF_IRQ_EN)
 
+#define TSC2007_LG_VDEF			1800 /* mV */
+#define TSC2007_LG_VMAX			4096 /* mV */
+#define TSC2007_LG_APROX		10000
+
+struct tsc2007_adc_devdata {
+	struct iio_chan_spec const *channels;
+	int num_channels;
+};
+
 struct tsc2007_adc {
 	struct i2c_client *client;
 	struct mutex		mlock;
 
 	u16			x_plate_ohms;
+
+	const struct tsc2007_adc_devdata *data;
 };
 
 struct ts_event {
 	u16	x;
 	u16	y;
 	u16	z1, z2;
+};
+
+struct lg_adc_graph {
+	int x;
+	int y;
+};
+
+static const struct lg_adc_graph battery_temp_graph[] = {
+	/* adc to temperature calibration table */
+	{ 1800, -50 }, { 1750, -40 }, { 1680, -30 },
+	{ 1585, -20 }, { 1445, -10 }, { 1273,   0 },
+	{ 1073,  10 }, {  855,  20 }, {  633,  30 },
+	{  498,  40 }, {  366,  50 }, {  290,  60 },
+	{  200,  70 }, {  150,  80 }, {  100,  90 },
+	{   80, 100 },
 };
 
 #define TSC2007_CHAN_IIO(_chan, _name, _type, _chan_info) \
@@ -66,6 +93,18 @@ static const struct iio_chan_spec tsc2007_adc_iio_channel[] = {
 	TSC2007_CHAN_IIO(5, "rt", IIO_VOLTAGE, IIO_CHAN_INFO_RAW),
 	TSC2007_CHAN_IIO(6, "temp0", IIO_TEMP, IIO_CHAN_INFO_RAW),
 	TSC2007_CHAN_IIO(7, "temp1", IIO_TEMP, IIO_CHAN_INFO_RAW),
+};
+
+static const struct iio_chan_spec tsc2007_adc_lg_iio_channel[] = {
+	TSC2007_CHAN_IIO(0, "x", IIO_VOLTAGE, IIO_CHAN_INFO_RAW),
+	TSC2007_CHAN_IIO(1, "y", IIO_VOLTAGE, IIO_CHAN_INFO_RAW),
+	TSC2007_CHAN_IIO(2, "z1", IIO_VOLTAGE, IIO_CHAN_INFO_RAW),
+	TSC2007_CHAN_IIO(3, "z2", IIO_VOLTAGE, IIO_CHAN_INFO_RAW),
+	TSC2007_CHAN_IIO(4, "adc", IIO_VOLTAGE, IIO_CHAN_INFO_RAW),
+	TSC2007_CHAN_IIO(5, "rt", IIO_VOLTAGE, IIO_CHAN_INFO_RAW),
+	TSC2007_CHAN_IIO(6, "temp0", IIO_TEMP, IIO_CHAN_INFO_RAW),
+	TSC2007_CHAN_IIO(7, "temp1", IIO_TEMP, IIO_CHAN_INFO_RAW),
+	TSC2007_CHAN_IIO(8, "temp3", IIO_TEMP, IIO_CHAN_INFO_RAW),
 };
 
 static int tsc2007_adc_xfer(struct tsc2007_adc *tsc2007, u8 cmd)
@@ -110,6 +149,25 @@ static u32 tsc2007_adc_calculate_resistance(struct tsc2007_adc *tsc2007, struct 
 	return rt;
 }
 
+static int lge_battery_ref(int adc, const struct lg_adc_graph *ref_graph, int size)
+{
+	int i = 1;
+	int slope, const_term;
+	int delta_y, delta_x;
+
+	while (adc < ref_graph[i].x && i < (size - 1))
+		i++;
+
+	delta_x = ref_graph[i-1].x - ref_graph[i].x;
+	delta_y = ref_graph[i-1].y - ref_graph[i].y;
+
+	slope = delta_y * TSC2007_LG_APROX / delta_x;
+
+	const_term = ref_graph[i].y - ref_graph[i].x * slope / TSC2007_LG_APROX;
+
+	return adc * slope / TSC2007_LG_APROX + const_term;
+}
+
 static int tsc2007_adc_read_raw(struct iio_dev *indio_dev,
 			    struct iio_chan_spec const *chan,
 			    int *val, int *val2, long mask)
@@ -118,7 +176,7 @@ static int tsc2007_adc_read_raw(struct iio_dev *indio_dev,
 	int adc_chan = chan->channel;
 	int ret = 0;
 
-	if (adc_chan >= ARRAY_SIZE(tsc2007_adc_iio_channel))
+	if (adc_chan >= indio_dev->num_channels)
 		return -EINVAL;
 
 	if (mask != IIO_CHAN_INFO_RAW)
@@ -159,6 +217,14 @@ static int tsc2007_adc_read_raw(struct iio_dev *indio_dev,
 		*val = tsc2007_adc_xfer(tsc2007,
 				    (ADC_ON_12BIT | TSC2007_MEASURE_TEMP1));
 		break;
+	case 8:
+		ret = tsc2007_adc_xfer(tsc2007, (ADC_ON_12BIT | TSC2007_MEASURE_AUX));
+
+		ret = (ret * TSC2007_LG_VDEF) / TSC2007_LG_VMAX;
+
+		*val = lge_battery_ref(ret, battery_temp_graph,
+				       ARRAY_SIZE(battery_temp_graph));
+		break;
 	}
 
 	/* Prepare for next touch reading - power down ADC, enable PENIRQ */
@@ -190,11 +256,15 @@ static int tsc2007_adc_probe(struct i2c_client *client,
 	tsc2007 = iio_priv(indio_dev);
 	tsc2007->client = client;
 
+	tsc2007->data = of_device_get_match_data(&client->dev);
+	if (!tsc2007->data)
+		return -ENODEV;
+
 	indio_dev->name = dev_name(&client->dev);
 	indio_dev->info = &tsc2007_adc_iio_info;
 	indio_dev->modes = INDIO_DIRECT_MODE;
-	indio_dev->channels = tsc2007_adc_iio_channel;
-	indio_dev->num_channels = ARRAY_SIZE(tsc2007_adc_iio_channel);
+	indio_dev->channels = tsc2007->data->channels;
+	indio_dev->num_channels = tsc2007->data->num_channels;
 
 	mutex_init(&tsc2007->mlock);
 
@@ -214,6 +284,16 @@ static int tsc2007_adc_probe(struct i2c_client *client,
 	return devm_iio_device_register(&client->dev, indio_dev);
 }
 
+static const struct tsc2007_adc_devdata lge_x3_tsc2007_data = {
+	.channels = tsc2007_adc_lg_iio_channel,
+	.num_channels = ARRAY_SIZE(tsc2007_adc_lg_iio_channel),
+};
+
+static const struct tsc2007_adc_devdata tsc2007_data = {
+	.channels = tsc2007_adc_iio_channel,
+	.num_channels = ARRAY_SIZE(tsc2007_adc_iio_channel),
+};
+
 static const struct i2c_device_id tsc2007_adc_id_table[] = {
 	{ "tsc2007_adc", 0 },
 	{ }
@@ -221,7 +301,8 @@ static const struct i2c_device_id tsc2007_adc_id_table[] = {
 MODULE_DEVICE_TABLE(i2c, tsc2007_adc_id_table);
 
 static const struct of_device_id tsc2007_adc_of_match[] = {
-	{ .compatible = "ti,tsc2007-adc" },
+	{ .compatible = "ti,tsc2007-adc", .data = &tsc2007_data },
+	{ .compatible = "lge-ti,tsc2007-adc", .data = &lge_x3_tsc2007_data },
 	{ /* sentinel */ }
 };
 MODULE_DEVICE_TABLE(of, tsc2007_adc_of_match);
