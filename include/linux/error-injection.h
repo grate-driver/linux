528b847971513c15a2d670bcc49fb3ecb2d03079
Stephen Rothwell <sfr@canb.auug.org.au>
2022-12-16 11:18:15 +1100

    Revert "error-injection: remove EI_ETYPE_NONE"
    This reverts commit fcb9954aa1dc85794501fd2d0b22efa150f44d62.

--- include/linux/error-injection.h
+++ include/linux/error-injection.h
@@ -19,7 +19,7 @@ static inline bool within_error_injection_list(unsigned long addr)
 
 static inline int get_injectable_error_type(unsigned long addr)
 {
-	return -EOPNOTSUPP;
+	return EI_ETYPE_NONE;
 }
 
 #endif
